b35d4508af6dc21a9c79c58edcc5dbea608eca0c
Piotr Kardaś <pkardas.it@gmail.com>
2020-10-10 16:24:32 +0200

    Reverted change in default_config.py

--- gupb/default_config.py
+++ gupb/default_config.py
@@ -1,21 +1,19 @@
 from gupb.controller import keyboard
 from gupb.controller import random
-from gupb.controller import botelka
 
 KEYBOARD_CONTROLLER = keyboard.KeyboardController()
-BOTELKA = botelka.BotElkaController("Elka")
 
 CONFIGURATION = {
     'arenas': [
         'island',
     ],
     'controllers': [
-        BOTELKA,
+        KEYBOARD_CONTROLLER,
         random.RandomController("Alice"),
         random.RandomController("Bob"),
         random.RandomController("Cecilia"),
     ],
     'visualise': True,
-    'show_sight': BOTELKA,
+    'show_sight': KEYBOARD_CONTROLLER,
     'runs_no': 1,
 }
