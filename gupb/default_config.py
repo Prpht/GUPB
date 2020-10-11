5cf2d7b897d4bc971ee06c0c4f6553fb30f26576
Koozco <kussy.kusy@gmail.com>
2020-10-12 00:03:56 +0200

    Checkout def config

--- gupb/default_config.py
+++ gupb/default_config.py
@@ -1,6 +1,5 @@
 from gupb.controller import keyboard
 from gupb.controller import random
-from gupb.controller import bb_bot
 
 KEYBOARD_CONTROLLER = keyboard.KeyboardController()
 
@@ -9,15 +8,12 @@ CONFIGURATION = {
         'island',
     ],
     'controllers': [
-        # KEYBOARD_CONTROLLER,
-        bb_bot.BBBotController("Bartek"),
+        KEYBOARD_CONTROLLER,
         random.RandomController("Alice"),
         random.RandomController("Bob"),
-        random.RandomController("Darius"),
         random.RandomController("Cecilia"),
     ],
-    'visualise': False,
-    # 'show_sight': KEYBOARD_CONTROLLER,
-    'show_sight': bb_bot.BBBotController("Bartek"),
-    'runs_no': 30,
+    'visualise': True,
+    'show_sight': KEYBOARD_CONTROLLER,
+    'runs_no': 1,
 }
