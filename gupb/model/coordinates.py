d8b3787ebfa81eb748a2f78e374adf773ca05450
Malenczuk <mmalenczukmarcin@gmail.com>
2020-10-13 21:51:25 +0200

    implement multiplication for Coords with scalars

--- gupb/model/coordinates.py
+++ gupb/model/coordinates.py
@@ -11,5 +11,13 @@ def sub_coords(self: Coords, other: Coords) -> Coords:
     return Coords(self[0] - other[0], self[1] - other[1])
 
 
+def mul_coords(self: Coords, other) -> Coords:
+    if isinstance(other, int):
+        return Coords(self[0] * other, self[1] * other)
+    else:
+        raise NotImplementedError
+
+
 Coords.__add__ = add_coords
 Coords.__sub__ = sub_coords
+Coords.__mul__ = mul_coords
