d949af617e7752bdaac13a7a9bb13dd2ca9ac162
kapiekarz <piekarz.karolina@gmail.com>
2020-10-18 15:40:15 +0200

    revert default_config.py to prev settings

--- gupb/default_config.py
+++ gupb/default_config.py
@@ -5,21 +5,19 @@ from gupb.controller import ihavenoideawhatimdoing
 from gupb.controller import krowa123
 from gupb.controller import tup_tup
 
-b = botelka.BotElkaController("Z nami na pewno zdasz")
-
 CONFIGURATION = {
     'arenas': [
         'island',
     ],
     'controllers': [
         bb_bot.BBBotController("Bartek"),
-        b,
+        botelka.BotElkaController("Z nami na pewno zdasz"),
         claret_wolf.ClaretWolfController(),
         ihavenoideawhatimdoing.IHaveNoIdeaWhatImDoingController(),
         krowa123.Krowa1233Controller("Krowka"),
         tup_tup.TupTupController('Bot'),
     ],
     'visualise': True,
-    'show_sight': b,
+    'show_sight': None,
     'runs_no': 1,
 }
