7c8003ff60ae392e28d2fccf239cb03350ba5cd2
boro <borkowskiszymon28@gmail.com>
2020-10-19 19:39:23 +0200

    Reverse changes to config

--- gupb/default_config.py
+++ gupb/default_config.py
@@ -1,7 +1,6 @@
-from gupb.controller import keyboard, shallow_mind
+from gupb.controller import keyboard
 from gupb.controller import random
 from gupb.controller import claret_wolf
-
 
 KEYBOARD_CONTROLLER = keyboard.KeyboardController()
 
@@ -11,11 +10,10 @@ CONFIGURATION = {
     ],
     'controllers': [
         KEYBOARD_CONTROLLER,
-        # claret_wolf.ClaretWolfController(),
-        # random.RandomController("Alice"),
-        # random.RandomController("Bob"),
-        # random.RandomController("Cecilia"),
-        shallow_mind.ShallowMindController('test'),
+        claret_wolf.ClaretWolfController(),
+        random.RandomController("Alice"),
+        random.RandomController("Bob"),
+        random.RandomController("Cecilia"),
     ],
     'visualise': True,
     'show_sight': KEYBOARD_CONTROLLER,
