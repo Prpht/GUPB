e5c875456db640dc36c80146c377393b29d3c7e3
boro <borkowskiszymon28@gmail.com>
2020-10-19 23:20:53 +0200

    Fixed blocking own character with weapon

--- gupb/controller/shallow_mind.py
+++ gupb/controller/shallow_mind.py
@@ -1,23 +1,12 @@
-import random
-
 from typing import Dict
-
 from pathfinding.finder.a_star import AStarFinder
 from pathfinding.core.grid import Grid
-
 from gupb.model import arenas, coordinates, tiles
 from gupb.model import characters
-from gupb.model.arenas import Terrain, terrain_size, Arena
+from gupb.model.arenas import Arena
 from gupb.model.characters import ChampionDescription
 from gupb.model.tiles import Menhir, Wall, Sea, Land
 from gupb.model.weapons import Knife, Sword, Bow, Axe, Amulet
-
-POSSIBLE_ACTIONS = [
-    characters.Action.TURN_LEFT,
-    characters.Action.TURN_RIGHT,
-    characters.Action.STEP_FORWARD,
-    characters.Action.ATTACK,
-]
 
 FIELD_WEIGHT = 100
 
@@ -26,8 +15,6 @@ TILES = [Land, Sea, Wall, Menhir]
 TILES_MAP = {tile().description().type: tile for tile in TILES}
 
 WEAPONS = [(Knife, 0), (Sword, FIELD_WEIGHT), (Bow, 1), (Axe, 5), (Amulet, 3)]
-
-WEAPONS_CLASSES = [x for x, _ in WEAPONS]
 
 WEAPONS_MAP = {weapon().description(): weapon for weapon, _ in WEAPONS}
 
@@ -55,17 +42,51 @@ class ArenaMapped(Arena):
                                                                          arena.terrain.items()}
         self.matrix = []
 
-    def prepare_matrix(self) -> None:
+    def get_champion(self, knowledge: characters.ChampionKnowledge) -> ChampionDescription:
+        return self.terrain.get(knowledge.position).character
+
+    def find_next_move(self, knowledge: characters.ChampionKnowledge) -> characters.Action:
         x_size, y_size = self.size
         self.matrix = [[FIELD_WEIGHT for _ in range(y_size)] for _ in range(x_size)]
+        champion = self.get_champion(knowledge)
+        used_weapon = WEAPONS_ENCODING.get(champion.weapon)
+        next_field = knowledge.position + champion.facing.value
+        self.matrix[next_field[1]][next_field[0]] = FIELD_WEIGHT / 2
+
+        for position, tileDescription in self.terrain.items():
+            y, x = position
+            if tileDescription.character and tileDescription.character != champion:
+                self.matrix[x][y] = -1
+                weapon = WEAPONS_MAP.get(tileDescription.character.weapon)
+                weapon.cut(self, position, tileDescription.character.facing)
+            elif self.matrix[x][y] > 0:
+                if tileDescription.loot:
+                    importance = WEAPONS_ENCODING.get(tileDescription.loot)
+                    if importance > used_weapon:
+                        self.matrix[x][y] = importance
+                    else:
+                        self.matrix[x][y] = -2
+                else:
+                    if not TILES_MAP.get(tileDescription.type).terrain_passable():
+                        self.matrix[x][y] = 0
+        grid = Grid(matrix=self.matrix)
+        start = grid.node(*knowledge.position)
+        end = grid.node(self.menhir_position[0], self.menhir_position[1] + 1)
+        path, _ = finder.find_path(start, end, grid)
+        print(grid.grid_str(path=path, start=start, end=end))
+        print(path)
+        print(knowledge.position)
+        print(champion.facing.value)
+        if len(path) > 1 and next_field == path[1]:
+            return characters.Action.STEP_FORWARD
+        else:
+            return characters.Action.TURN_LEFT
 
     def register_effect(self, _, coords: coordinates.Coords) -> None:
-        x, y = coords
+        y, x = coords
         self.matrix[x][y] = -1
 
 
-# noinspection PyUnusedLocal
-# noinspection PyMethodMayBeStatic
 class ShallowMindController:
     def __init__(self, first_name: str):
         self.first_name: str = first_name
@@ -80,50 +101,15 @@ class ShallowMindController:
     def __hash__(self) -> int:
         return hash(self.first_name)
 
-    def __get_champion(self, knowledge: characters.ChampionKnowledge) -> ChampionDescription:
-        return self.arena.terrain.get(knowledge.position).character
-
     def reset(self, arena_description: arenas.ArenaDescription) -> None:
         arena = arenas.Arena.load(arena_description.name)
         self.arena = ArenaMapped(arena)
         self.arena.menhir_position = arena_description.menhir_position
 
     def decide(self, knowledge: characters.ChampionKnowledge) -> characters.Action:
-        arena = self.arena
-        arena.terrain = {**arena.terrain, **knowledge.visible_tiles}
-        champ = self.__get_champion(knowledge)
-        used_weapon = WEAPONS_ENCODING.get(champ.weapon)
+        self.arena.terrain = {**self.arena.terrain, **knowledge.visible_tiles}
         # It's assumed that the whole map is known
-        self.arena.prepare_matrix()
-        next_field = knowledge.position + champ.facing.value
-        self.arena.matrix[next_field[1]][next_field[0]] = FIELD_WEIGHT / 2
-
-        for position, tileDescription in arena.terrain.items():
-            y, x = position
-            if tileDescription.character:
-                arena.matrix[x][y] = -1
-                weapon = WEAPONS_MAP.get(tileDescription.character.weapon)
-                weapon.cut(self.arena, position, tileDescription.character.facing)
-            elif arena.matrix[x][y] > 0:
-                if tileDescription.loot:
-                    importance = WEAPONS_ENCODING.get(tileDescription.loot)
-                    if importance > used_weapon:
-                        arena.matrix[x][y] = importance
-                    else:
-                        arena.matrix[x][y] = -2
-                else:
-                    if not TILES_MAP.get(tileDescription.type).terrain_passable():
-                        arena.matrix[x][y] = 0
-
-        grid = Grid(matrix=self.arena.matrix)
-        start = grid.node(*knowledge.position)
-        end = grid.node(self.arena.menhir_position[0], self.arena.menhir_position[1]+1)
-        path, runs = finder.find_path(start, end, grid)
-        self.prev_champion = champ
-        if len(path) > 1 and next_field == path[1]:
-            return characters.Action.STEP_FORWARD
-        else:
-            return characters.Action.TURN_LEFT
+        return self.arena.find_next_move(knowledge)
 
     @property
     def name(self) -> str:
