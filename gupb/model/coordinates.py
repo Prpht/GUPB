7654e52b03b7962e2fb1f77dce27a9b6a35f17ea
Malenczuk <mmalenczukmarcin@gmail.com>
2020-10-20 17:10:13 +0200

    fix coordinates equals

--- gupb/model/coordinates.py
+++ gupb/model/coordinates.py
@@ -10,11 +10,6 @@ def add_coords(self: Coords, other: Coords) -> Coords:
 def sub_coords(self: Coords, other: Coords) -> Coords:
     return Coords(self[0] - other[0], self[1] - other[1])
 
-def mul_coords(self: Coords, other: int) -> Coords:
-    return Coords(self[0] * other, self[1] * other)
-
-def eq_coords(self: Coords, other: Coords) -> bool:
-    return self[0] == other[0] and self[1] == other[1]
 
 def mul_coords(self: Coords, other) -> Coords:
     if isinstance(other, int):
@@ -26,4 +21,3 @@ def mul_coords(self: Coords, other) -> Coords:
 Coords.__add__ = add_coords
 Coords.__sub__ = sub_coords
 Coords.__mul__ = mul_coords
-Coords.__eq__ = eq_coords
