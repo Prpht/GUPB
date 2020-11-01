9dd84cdc3111d69623489a9159de7f3ce2e41a7e
rpych <dragon0710@wp.pl>
2020-11-01 18:47:10 +0100

    get back settings from config

--- gupb/default_config.py
+++ gupb/default_config.py
@@ -6,23 +6,19 @@ from gupb.controller import krowa123
 from gupb.controller import tup_tup
 from gupb.controller import random
 
-CLARET_WOLF = claret_wolf.ClaretWolfController()
-
 CONFIGURATION = {
     'arenas': [
         'island',
     ],
     'controllers': [
-        #bb_bot.BBBotController("Bartek"),
-        #botelka.BotElkaController("Z nami na pewno zdasz"),
-        CLARET_WOLF,
-        random.RandomController("FIRST1"),
-        random.RandomController("SECOND2")
-        #ihavenoideawhatimdoing.IHaveNoIdeaWhatImDoingController(),
-        #krowa123.Krowa1233Controller("Krowka"),
-        #tup_tup.TupTupController('Bot'),
+        bb_bot.BBBotController("Bartek"),
+        botelka.BotElkaController("Z nami na pewno zdasz"),
+        claret_wolf.ClaretWolfController(),
+        ihavenoideawhatimdoing.IHaveNoIdeaWhatImDoingController(),
+        krowa123.Krowa1233Controller("Krowka"),
+        tup_tup.TupTupController('Bot'),
     ],
     'visualise': True,
-    'show_sight': CLARET_WOLF,
+    'show_sight': None,
     'runs_no': 1,
 }
