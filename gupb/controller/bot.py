1d543f2e7c21d3259c278a1f323b50729d895fa2
Jan <jan.ilczuk@gmail.com>
2021-10-14 16:19:41 +0200

    Refactor code

--- gupb/controller/bot.py
+++ gupb/controller/bot.py
@@ -32,9 +32,7 @@ class BotController:
         self.facing = None  # inicjalizacja przy pierwszym decide
         self.position = None  # inicjalizacja przy pierwszym decide
         self.menhir_coord: coordinates.Coords = None
-        self.map = {}
-        self.max_y = 0
-        self.max_x = 0
+        self.grid = {}
         self.reached_menhir = False
 
     def __eq__(self, other: object) -> bool:
@@ -58,7 +56,10 @@ class BotController:
             return characters.Action.ATTACK
 
         if not self.reached_menhir:
-            self.__astar()
+            path = Astar.astar(self.grid, self.position, self.menhir_coord)
+            if path is not None:
+                self.action_queue = self.__generate_queue_from_path(path[:-1]) # without last element, because last element is menhir
+                self.reached_menhir = True
 
         if not self.action_queue.empty():
             return self.action_queue.get()
@@ -82,12 +83,7 @@ class BotController:
         character = knowledge.visible_tiles[self.position].character
         self.facing = character.facing
         self.current_weapon = character.weapon.name
-        self.map.update(knowledge.visible_tiles)
-        for coord in self.map.keys():
-            if coord[0] > self.max_x:
-                self.max_x = coord[0]
-            if coord[1] > self.max_y:
-                self.max_y = coord[1]
+        self.grid.update(knowledge.visible_tiles)
 
     def __can_attack(self, visible_tiles: Dict[coordinates.Coords, tiles.TileDescription]) -> bool:
         try:
@@ -122,67 +118,6 @@ class BotController:
     def __should_reload(self):
         return self.current_weapon == 'bow_unloaded'
 
-    def __astar(self):
-        start_node = Node(None, self.position)
-        end_node = Node(None, self.menhir_coord)
-
-        open_list = []
-        closed_list = []
-        open_list.append(start_node)
-        while len(open_list) > 0:
-            open_list.sort()
-            # Get the current node
-            current_node = open_list.pop(0)
-            # Pop current off open list, add to closed list
-            closed_list.append(current_node)
-
-            # Found the goal
-            if current_node == end_node:
-                self.reached_menhir = True
-                path = []
-                current = current_node.parent  # parent, a nie current node, poniewaz nie mozemy wejsc ma mnehira
-                while current != start_node:
-                    path.append(current.position)
-                    current = current.parent
-                self.action_queue = self.__generate_queue_from_path(path[::-1])  # Return reversed path
-
-            # Generate children
-            for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0)]:  # Adjacent squares
-
-                # Get node position
-                node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
-
-                # Make sure within range
-                if node_position[0] > self.max_x or node_position[0] < 0 or node_position[1] > self.max_y or \
-                        node_position[1] < 0:
-                    continue
-                #
-                # # Make sure walkable terrain
-                if self.map.get(node_position) is None or self.map[node_position].type in ['sea', 'wall']:
-                    continue
-
-                # Create new node
-                neighbor = Node(current_node, node_position)
-                # Check if the neighbor is in the closed list
-                if neighbor in closed_list:
-                    continue
-                # Generate heuristics (Manhattan distance)
-                neighbor.g = abs(neighbor.position[0] - start_node.position[0]) + abs(
-                    neighbor.position[1] - start_node.position[1])
-                neighbor.h = abs(neighbor.position[0] - end_node.position[0]) + abs(
-                    neighbor.position[1] - end_node.position[1])
-                neighbor.f = neighbor.g + neighbor.h
-                # Check if neighbor is in open list and if it has a lower f value
-                if self.__add_to_open(open_list, neighbor):
-                    # Everything is green, add neighbor to open list
-                    open_list.append(neighbor)
-
-    def __add_to_open(self, open_list, neighbor):
-        for node in open_list:
-            if neighbor == node and neighbor.f >= node.f:
-                return False
-        return True
-
     def __generate_queue_from_path(self, path):
         queue = SimpleQueue()
         current_cord = self.position
@@ -215,6 +150,64 @@ class BotController:
             current_facing = desired_facing
         return queue
 
+
+class Astar:
+    @staticmethod
+    def astar(grid, start_position, end_position):
+        start_node = Node(None, start_position)
+        end_node = Node(None, end_position)
+
+        open_list = []
+        closed_list = []
+        open_list.append(start_node)
+        while len(open_list) > 0:
+            open_list.sort()
+            # Get the current node
+            current_node = open_list.pop(0)
+            # Pop current off open list, add to closed list
+            closed_list.append(current_node)
+
+            # Found the goal
+            if current_node == end_node:
+                path = []
+                current = current_node  # parent, a nie current node, poniewaz nie mozemy wejsc ma mnehira
+                while current != start_node:
+                    path.append(current.position)
+                    current = current.parent
+                return path[::-1]
+
+            # Generate children
+            for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0)]:  # Adjacent squares
+
+                # Get node position
+                node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+
+                # # Make sure walkable terrain
+                if grid.get(node_position) is None or grid[node_position].type in ['sea', 'wall']:
+                    continue
+
+                # Create new node
+                neighbor = Node(current_node, node_position)
+                # Check if the neighbor is in the closed list
+                if neighbor in closed_list:
+                    continue
+                # Generate heuristics (Manhattan distance)
+                neighbor.g = abs(neighbor.position[0] - start_node.position[0]) + abs(
+                    neighbor.position[1] - start_node.position[1])
+                neighbor.h = abs(neighbor.position[0] - end_node.position[0]) + abs(
+                    neighbor.position[1] - end_node.position[1])
+                neighbor.f = neighbor.g + neighbor.h
+                # Check if neighbor is in open list and if it has a lower f value
+                if Astar.__add_to_open(open_list, neighbor):
+                    # Everything is green, add neighbor to open list
+                    open_list.append(neighbor)
+
+    @staticmethod
+    def __add_to_open(open_list, neighbor):
+        for node in open_list:
+            if neighbor == node and neighbor.f >= node.f:
+                return False
+        return True
 
 class Node():
     def __init__(self, parent=None, position=None):
