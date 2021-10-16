9fc565dbdf041465938521a183296986b06c12de
Satoszi <49093488+Satoszi@users.noreply.github.com>
2021-10-16 22:25:04 +0200

    Dragon3000 v1.0 (#63)

--- gupb/default_config.py
+++ gupb/default_config.py
@@ -1,18 +1,21 @@
 from gupb.controller import random
+from gupb.controller import bot
+from gupb.controller import keyboard
 
 CONFIGURATION = {
     'arenas': [
         'fisher_island',
     ],
     'controllers': [
-        random.RandomController("Alice"),
+        bot.BotController("Alice"),
         random.RandomController("Bob"),
         random.RandomController("Cecilia"),
         random.RandomController("Darius"),
+        keyboard.KeyboardController(),
     ],
     'start_balancing': False,
-    'visualise': True,
+    'visualise': False,
     'show_sight': None,
-    'runs_no': 300,
+    'runs_no': 20,
     'profiling_metrics': [],  # possible metrics ['all', 'total', 'avg']
 }
