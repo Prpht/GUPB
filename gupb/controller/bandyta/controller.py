5f61e0c7b7c5009881564dd3dddf950d3f0c94ab
domo <domo2333@gmail.com>
2021-10-17 12:03:52 +0200

    Improve BFS algorithm

--- gupb/controller/bandyta/controller.py
+++ gupb/controller/bandyta/controller.py
@@ -1,23 +1,26 @@
+from __future__ import annotations
 import random
-from typing import Dict, List
+from typing import Dict
 
-from gupb.controller.bandyta.utils import POSSIBLE_ACTIONS, get_distance, get_direction, Direction, left_walk
+from gupb.controller.bandyta.utils import POSSIBLE_ACTIONS, get_direction, Direction, Path, \
+    find_target_player, is_attack_possible, find_furthest_point, find_menhir
 from gupb.model import characters, tiles
 from gupb.model.characters import ChampionKnowledge
 from gupb.model import arenas
 from gupb.model.coordinates import Coords
 from gupb.controller.bandyta.bfs import find_path
-from gupb.model.coordinates import sub_coords
-from math import fabs
-
-from gupb.model.weapons import WeaponDescription
 
 
 class Bandyta:
+    """
+    Dziary na pół ryja...
+    """
+
     def __init__(self, first_name: str):
         self.first_name: str = first_name
         self.landscape_map: Dict[int, Dict[int, str]] = {}
-        self.path: List[Coords] = []
+        self.path = Path(None, [])
+        self.menhir: Coords | None = None
 
     def __eq__(self, other: object):
         if isinstance(other, Bandyta):
@@ -27,43 +30,36 @@ class Bandyta:
     def __hash__(self):
         return hash(self.first_name)
 
-    def attack_possible(self, my_coord: Coords, enemy_coord: Coords, weapon: WeaponDescription) -> bool:
-        sub = sub_coords(my_coord, enemy_coord)
-        if weapon.name == 'sword':
-            return (fabs(sub.x) <= 3 and sub.y == 0) or (fabs(sub.y) <= 3 and sub.x == 0)
-        elif weapon.name == 'bow':
-            return (fabs(sub.x) <= 50 and sub.y == 0) or (fabs(sub.y) <= 50 and sub.x == 0)
-        else:
-            return (fabs(sub.x) == 1 and sub.y == 0) or (fabs(sub.y) == 1 and sub.x == 0)
-
     def decide(self, knowledge: ChampionKnowledge):
         try:
             self.memorize_landscape(knowledge)
             direction = get_direction(knowledge)
-            player = self.find_player(knowledge.visible_tiles)
-            menhir = self.find_menhir(knowledge.visible_tiles)
-            if player is not None and self.attack_possible(knowledge.position, player, self.get_my_weapon(knowledge.visible_tiles)):
-                self.path = []
+            player = find_target_player(self.name, knowledge, self.path.dest)
+            self.menhir = find_menhir(knowledge.visible_tiles) if self.menhir is None else self.menhir
+
+            if player is not None and \
+                    is_attack_possible(knowledge.position, player[1], self.get_my_weapon(knowledge.visible_tiles)):
+                self.path = Path('', [])
                 return characters.Action.ATTACK
 
-            if player is not None or menhir is not None or len(self.path) != 0:
-                if len(self.path) == 0:
-                    if player is not None and get_distance(knowledge.position, player) < 8:
-                        self.path = find_path(knowledge.position, player, self.landscape_map)
-                        if len(self.path) != 0:
-                            self.path.pop(0)
+            if player is not None and (len(self.path.route) == 0 or self.path.dest is player[0]):
+                self.path = Path(player[0], find_path(knowledge.position, player[1], self.landscape_map))
 
-                    if menhir is not None and get_distance(knowledge.position, menhir) < 8:
-                        self.path = find_path(knowledge.position, menhir, self.landscape_map)
-                        if len(self.path) != 0:
-                            self.path.pop(0)
+            if len(self.path.route) == 0 and self.menhir is not None:
+                self.path = Path('menhir', find_path(knowledge.position, self.menhir, self.landscape_map))
 
-                if len(self.path) != 0:
-                    action = self.move_on_path(knowledge.position, direction)
-                    return action
+            if len(self.path.route) == 0 and self.menhir is None:
+                self.path = Path('furthest_point',
+                                 find_path(knowledge.position,
+                                           find_furthest_point(knowledge),
+                                           self.landscape_map))
+
+            if len(self.path.route) != 0:
+                return self.move_on_path(knowledge.position, direction)
+
             return random.choice(POSSIBLE_ACTIONS)
         except Exception as e:
-            print(e)
+            # print(e)
             return random.choice(POSSIBLE_ACTIONS)
 
     def reset(self, arena_description: arenas.ArenaDescription):
@@ -88,26 +84,14 @@ class Bandyta:
                 self.landscape_map[cords[0]] = {}
             self.landscape_map[cords[0]][cords[1]] = tile.type
 
-    def find_player(self, visible_tiles: Dict[Coords, tiles.TileDescription]):
-        for cords, tile in visible_tiles.items():
-            if (tile.character is not None and tile.character.controller_name != self.name): #or tile.type == 'menhir':
-                return cords
-        return None
-
-    def find_menhir(self, visible_tiles: Dict[Coords, tiles.TileDescription]):
-        for cords, tile in visible_tiles.items():
-            if tile.type == 'menhir':
-                return cords
-        return None
-
     def move_on_path(self, position: Coords, direction: Direction) -> characters.Action:
-        if position + direction.value == self.path[0]:
-            self.path.pop(0)
+        if position + direction.value == self.path.route[0]:
+            self.path.route.pop(0)
             return characters.Action.STEP_FORWARD
         else:
             return characters.Action.TURN_LEFT
 
 
 POTENTIAL_CONTROLLERS = [
-    Bandyta('test'),
+    Bandyta('v1.2'),
 ]
--- gupb/controller/bandyta/utils.py
+++ gupb/controller/bandyta/utils.py
@@ -1,8 +1,11 @@
 from enum import Enum
+from math import fabs
+from typing import NamedTuple, List, Optional, Dict
 
-from gupb.model import characters
+from gupb.model import characters, tiles
 from gupb.model.characters import ChampionKnowledge
-from gupb.model.coordinates import Coords
+from gupb.model.coordinates import Coords, sub_coords
+from gupb.model.weapons import WeaponDescription
 
 
 class Direction(Enum):
@@ -10,6 +13,9 @@ class Direction(Enum):
     W = Coords(-1, 0)
     N = Coords(0, -1)
     E = Coords(1, 0)
+
+
+Path = NamedTuple('Path', [('dest', Optional[str]), ('route', List[Coords])])
 
 
 def get_direction(knowledge: ChampionKnowledge) -> Direction:
@@ -27,6 +33,60 @@ def left_walk(knowledge: ChampionKnowledge, direction: Direction) -> characters.
         return characters.Action.STEP_FORWARD
     else:
         return characters.Action.TURN_LEFT
+
+
+def is_attack_possible(my_coord: Coords, enemy_coord: Coords, weapon: WeaponDescription) -> bool:
+    sub = sub_coords(my_coord, enemy_coord)
+    if weapon.name == 'sword':
+        return (fabs(sub.x) <= 3 and sub.y == 0) or (fabs(sub.y) <= 3 and sub.x == 0)
+    elif weapon.name == 'bow':
+        return (fabs(sub.x) <= 50 and sub.y == 0) or (fabs(sub.y) <= 50 and sub.x == 0)
+    else:
+        return (fabs(sub.x) == 1 and sub.y == 0) or (fabs(sub.y) == 1 and sub.x == 0)
+
+
+def find_players(name: str, visible_tiles: Dict[Coords, tiles.TileDescription]):
+    players: Dict[str, Coords] = {}
+    for cords, tile in visible_tiles.items():
+        if (tile.character is not None and
+                tile.character.controller_name != name):
+            players[tile.character.controller_name] = cords
+    return players
+
+
+def find_closest_player(name: str, knowledge: ChampionKnowledge):
+    players = find_players(name, knowledge.visible_tiles)
+    if len(players) == 0:
+        return None
+
+    distances = {}
+    for player in players.items():
+        distances[player[0]] = get_distance(knowledge.position, player[1])
+
+    player = min(distances, key=distances.get)
+    return player, players[player]
+
+
+def find_target_player(name: str, knowledge: ChampionKnowledge, target: str):
+    players = find_players(name, knowledge.visible_tiles)
+    return (target, players[target]) if target in players.keys() else \
+        (list(players.keys())[0], list(players.values())[0]) if len(players) > 0 else None
+
+
+def find_menhir(visible_tiles: Dict[Coords, tiles.TileDescription]):
+    for cords, tile in visible_tiles.items():
+        if tile.type == 'menhir':
+            return cords
+    return None
+
+
+def find_furthest_point(knowledge: ChampionKnowledge):
+    furthest_point = knowledge.position
+    for tile, desc in knowledge.visible_tiles:
+        furthest_point = tile if \
+            desc is 'land' and get_distance(knowledge.position, tile) > get_distance(knowledge.position, furthest_point) else \
+            furthest_point
+    return furthest_point
 
 
 POSSIBLE_ACTIONS = [
