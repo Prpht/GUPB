9ad21c3accffc87c0dbda0b54863d1d8c1f6cf13
Filip Komarzyniec <fkomarzy@akamai.com>
2021-10-24 00:18:34 +0200

    merged Artur work w/ mine

--- gupb/controller/marwin.py
+++ gupb/controller/marwin.py
@@ -3,6 +3,8 @@ import math, random
 from gupb.model import arenas
 from gupb.model import characters
 from typing import NamedTuple, Optional
+from gupb.model.coordinates import Coords
+from collections import deque
 
 POSSIBLE_ACTIONS = [
     characters.Action.TURN_LEFT,
@@ -11,21 +13,140 @@ POSSIBLE_ACTIONS = [
     characters.Action.DO_NOTHING,
 ]
 
+WEAPONS_ORDER = {  ## which weapon is better for the current map (isolated_shrine)
+    "Knife": 0,
+    "Bow": 2,
+    "Axe":  5,
+    "Amulet": 4,
+    "Sword": 3,
+}
 
-class EvaderController:
+BLOCKERS = ["=", "#"]
 
+class BaseMarwinController:
     def __init__(self, first_name: str):
-        self.first_name: str = first_name
+        self.first_name = first_name
         self._initial_health = characters.CHAMPION_STARTING_HP
         self._current_facing = None
 
     def __eq__(self, other: object) -> bool:
-        if isinstance(other, EvaderController):
+        if isinstance(other, BaseMarwinController):
             return True
         return False
 
     def __hash__(self) -> int:
         return hash(self.first_name)
+
+    @property
+    def name(self) -> str:
+        return f'{self.__class__.__name__}--{self.first_name}'
+
+    @property
+    def preferred_tabard(self) -> characters.Tabard:
+        return characters.Tabard.VIOLET
+
+    @staticmethod
+    def _get_weapon_for_description(weapon_description: str):
+        from gupb.model import weapons
+
+        weapons_cls = {
+            'knife': weapons.Knife,
+            'sword': weapons.Sword,
+            'bow': weapons.Bow,
+            'axe': weapons.Axe,
+            'amulet': weapons.Amulet
+        }
+        for key, weapon in weapons_cls.items():
+            if key in weapon_description.lower():
+                return weapon
+        return None
+
+class WiseTankController(BaseMarwinController):
+    def __init__(self, first_name):
+        super(WiseTankController, self).__init__(first_name)
+        self.arena = self._parse_arena()
+        self.precalculated_path = None
+        self.next_move = None
+
+    def reset(self, arena_description: arenas.ArenaDescription) -> None:
+        pass
+
+    def decide(self, knowledge: characters.ChampionKnowledge) -> characters.Action:
+        ## first of all we want to be as close to the fixed menhir as possible, because this way we can avoid mist in the long run
+        '''if menhir visible: set necessary steps to go there
+            else: set path to menhir and cache it
+            if enemy in sight and in range of currently equipped weapon: attack
+            elif we see better weapon than currently equipped:
+                calculate distance to that
+                if distance is not too far from our path to menhir:
+                    correct path to the menhir so that we pick up that weapon
+        '''
+        tiles = knowledge.visible_tiles
+        my_position = knowledge.position
+        my_character = knowledge.visible_tiles[my_position].character
+        action = characters.Action.DO_NOTHING
+        if not self.precalculated_path:
+            self.precalculated_path = self.find_path(my_position, (9,9))[1:]  # without position we're standing on
+        #else:
+        if not self.next_move:
+            self.next_move = self.precalculated_path[0]
+        # gdzie leży płytka względem mnie
+
+        if self.next_move.x - my_position.x < 0: next_facing = characters.Facing.LEFT # UP
+        elif self.next_move.x - my_position.x > 0: next_facing = characters.Facing.RIGHT # DOWN
+        elif self.next_move.y - my_position.y < 0: next_facing = characters.Facing.UP # LEFT
+        else:  next_facing = characters.Facing.DOWN  # RIGHT
+
+        if next_facing == my_character.facing:
+            action = characters.Action.STEP_FORWARD
+            self.next_move = None
+            del self.precalculated_path[0]
+
+        elif ( next_facing == characters.Facing.LEFT and my_character.facing == characters.Facing.UP or
+                next_facing == characters.Facing.UP and my_character.facing == characters.Facing.RIGHT or
+                next_facing == characters.Facing.RIGHT and my_character.facing == characters.Facing.DOWN or
+                next_facing == characters.Facing.DOWN and my_character.facing == characters.Facing.LEFT ):
+            action = characters.Action.TURN_RIGHT
+        else:
+            action = characters.Action.TURN_LEFT
+
+        return action
+
+        # for tile_coords, tile_desc in tiles.items():
+        #     if tile_desc.type == "Menhir":
+                # calculate distance to menhir
+            #elif tile_desc.character and  tile_desc.character != my_character:  #enemy in sight
+                #calculate whether enemy in distance of currently equipped weapon
+                # if so, hit
+            #elif WEAPONS_ORDER[tile_desc.loot.name] > WEAPONS_ORDER[my_character.weapon.name]: # we see better weapon
+                # calculate path to weapon, if not long, update path to menhir
+
+    def _parse_arena(self):
+        with open("./resources/arenas/isolated_shrine.gupb", 'r') as f:
+            return [row for row in f.readlines()]
+
+
+    def _scan_environment(self, ) :
+        pass
+
+    def find_path(self, start, dst):
+        width = height = 19
+        queue = deque([[start]])
+        seen = set([start])  # set literal
+        while queue:
+            path = queue.popleft()
+            tile = path[-1]
+            if (tile.y, tile.x) == dst:
+                return path
+            for x2, y2 in ((tile.x + 1, tile.y), (tile.x - 1, tile.y), (tile.x, tile.y + 1), (tile.x, tile.y - 1)):
+                if 0 <= x2 < width and 0 <= y2 < height and self.arena[x2][y2] not in BLOCKERS and (x2, y2) not in seen:
+                    queue.append(path + [Coords(x2, y2)])
+                    seen.add(Coords(x2, y2))
+
+class EvaderController(BaseMarwinController):
+
+    def __init__(self, first_name: str):
+        super().__init__(first_name)
 
     def reset(self, arena_description: arenas.ArenaDescription) -> None:
         pass
@@ -84,31 +205,9 @@ class EvaderController:
                 nearest = distance
         return nearest
 
-    @property
-    def name(self) -> str:
-        return f'EvaderController{self.first_name}'
-
-    @property
-    def preferred_tabard(self) -> characters.Tabard:
-        return characters.Tabard.VIOLET
-    
-    @staticmethod
-    def _get_weapon_for_description(weapon_description: str):
-        from gupb.model import weapons
-
-        weapons_cls = {
-            'knife': weapons.Knife,
-            'sword': weapons.Sword,
-            'bow': weapons.Bow,
-            'axe': weapons.Axe,
-            'amulet': weapons.Amulet
-        }
-        for key, weapon in weapons_cls.items():
-            if key in weapon_description.lower():
-                return weapon
-        return None
 
 
+## Auxiliary methods
 def _is_tile_transparent(tile_name):
     from gupb.model import tiles
     tiles_cls = {
@@ -138,5 +237,6 @@ def _get_cut_positions(weapon_cls, terrain, position, facing):
 
 
 POTENTIAL_CONTROLLERS = [
+    WiseTankController("MarwinWise"),
     EvaderController("Marwin"),
 ]
