0aef5c7644a12745b3c2597b1d2f342f2bf337b8
Filip Ślazyk <filip.slazyk@gmail.com>
2021-11-10 19:03:12 +0100

    Implement explore state

--- gupb/controller/wietnamczyk.py
+++ gupb/controller/wietnamczyk.py
@@ -11,46 +11,32 @@ from gupb.model.arenas import Arena
 from gupb.model.characters import Facing
 from gupb.model.coordinates import Coords
 
+from typing import List, Dict
+
 
 # noinspection PyUnusedLocal
 # noinspection PyMethodMayBeStatic
 class WIETnamczyk(controller.Controller):
-    FIND_WEAPON = "find_weapon"
-
-    # czy zarezerwowano caly przedzial dla psa?
-    KRAZ_MIEDZY_SAFEPOINTAMI = "travel_to_safepoints"
-    FIND_MENHIR = "find_menhir"
+    GET_WEAPON = "get_weapon"
+    EXPLORE = "explore"
+    PANIC = "panic"
 
     def __init__(self):
-        self.outer_places = [
-            Coords(6, 6),
-            Coords(6, 12),
-            Coords(12, 6),
-            Coords(12, 12)
-        ]
-        self.outer_prob = [0.2, 0.3, 0.3, 0.2]
-        self.inner_places = [
-            Coords(8, 8),
-            Coords(8, 10),
-            Coords(9, 9),
-            Coords(10, 8),
-            Coords(10, 10)
-        ]
-        self.inner_prob = [0.17, 0.18, 0.3, 0.18, 0.17]
-        self.safe_places = self.outer_places
-        self.prob = self.outer_prob
         self.mist_range = 3
+        self.enemy_range = 8
 
-        self.menhir_pos = Coords(9, 9)
+        self.menhir_pos = None
         self.good_weapons = ["sword", "axe"]
         self.first_name: str = "Adam"
-        self.map = self.parse_map()
+        self.map: List[List[tiles.TileDescription]] = self.parse_map()
+        self.unseen_coords = self.generate_coords()
         self.arena_description = None
         self.current_weapon = "knife"
-        self.state = WIETnamczyk.FIND_WEAPON
+        self.state = WIETnamczyk.GET_WEAPON
         self.next_dest = None
         self.hp = None
         self.facing = None
+        self.exploration_goal = None
 
     def dist(self, tile1: coordinates.Coords, tile2: coordinates.Coords):
         dist = abs(tile1[0] - tile2[0]) + abs(tile1[1] - tile2[1])
@@ -140,6 +126,15 @@ class WIETnamczyk(controller.Controller):
                        key=lambda item: item[1]))
         return closest_good_weapon[0][0]
 
+    def find_visible_enemies(self, bot_pos, visible_tiles: Dict[coordinates.Coords, tiles.TileDescription], ):
+        enemies_list = []
+        for tile, description in visible_tiles.items():
+            if description.character is not None:
+                dist_to_enemy = len(self.find_path(bot_pos, tile))
+                if dist_to_enemy <= self.enemy_range:
+                    enemies_list.append((description, tile))
+        return enemies_list
+
     def find_direction(self, path_to_destination, knowledge, bot_pos):
         if len(path_to_destination) == 0:
             return random.choice([characters.Action.TURN_RIGHT, characters.Action.TURN_LEFT])
@@ -183,18 +178,29 @@ class WIETnamczyk(controller.Controller):
 
     def update_knowledge(self, visible_tiles, bot_pos):
         for tile, description in visible_tiles.items():
+            self.unseen_coords.remove(tile)
             self.map[tile[0]][tile[1]] = description
+            if description.type == 'menhir':
+                self.menhir_pos = tile
             if self.dist(tile, bot_pos) == 0:
                 self.current_weapon = description.character.weapon
                 self.hp = description.character.health
                 self.facing = description.character.facing
 
-    def parse_map(self):
+    def parse_map(self) -> List[List[tiles.TileDescription]]:
         arena = Arena.load("isolated_shrine")
         map_matrix = [[None for i in range(arena.size[0])] for j in range(arena.size[1])]
         for k, v in arena.terrain.items():
             map_matrix[k[0]][k[1]] = v.description()
         return map_matrix
+
+    def generate_coords(self):
+        unseen_coords = set()
+        for i, row in enumerate(self.map):
+            for j, cell in enumerate(row):
+                if cell.type in {'land', 'menhir'}:
+                    unseen_coords.add(Coords(i, j))
+        return unseen_coords
 
     def find_path(self, start_pos, dest_coord):
         X = len(self.map)
@@ -240,27 +246,38 @@ class WIETnamczyk(controller.Controller):
         bot_pos = knowledge.position
         self.update_knowledge(knowledge.visible_tiles, bot_pos)
         self.evaluate_mist(bot_pos, knowledge)
+
         if self.should_attack(bot_pos, knowledge):
             return characters.Action.ATTACK
 
-        if self.state == WIETnamczyk.FIND_WEAPON:
+        if self.state == WIETnamczyk.EXPLORE:
+            visible_enemies = self.find_visible_enemies(bot_pos, knowledge.visible_tiles)
+            if len(visible_enemies) > 1:
+                # TODO implement enemy avoidance
+                return characters.Action.TURN_RIGHT
+            if len(visible_enemies) == 1:
+                enemy_pos = visible_enemies[0][1]
+                enemy_tile_description = visible_enemies[0][0]
+                if self.should_fight(bot_pos, enemy_pos, enemy_tile_description):
+                    path_to_destination = self.find_path((bot_pos[0], bot_pos[1]), (enemy_pos[0], enemy_pos[1]))
+                    return self.find_direction(path_to_destination, knowledge, bot_pos)
+            return self.explore_map(bot_pos, knowledge)
+
+        if self.state == WIETnamczyk.GET_WEAPON:
             weapon_pos = self.find_good_weapon(bot_pos)
             if not weapon_pos or self.current_weapon.name in self.good_weapons:
-                self.state = WIETnamczyk.KRAZ_MIEDZY_SAFEPOINTAMI
+                self.state = WIETnamczyk.EXPLORE
             else:
                 path_to_destination = self.find_path((bot_pos[0], bot_pos[1]), (weapon_pos[0], weapon_pos[1]))
                 return self.find_direction(path_to_destination, knowledge, bot_pos)
 
-        if self.state == WIETnamczyk.KRAZ_MIEDZY_SAFEPOINTAMI:
-            if not self.next_dest or self.dist(self.next_dest, bot_pos) < 1:
-                dest = self.get_random_safe_place(bot_pos)
-                if dest[0] == bot_pos[0] and dest[1] == bot_pos[1]:
-                    return characters.Action.TURN_RIGHT
-                self.next_dest = dest
-            path_to_destination = self.find_path((bot_pos[0], bot_pos[1]), (self.next_dest[0], self.next_dest[1]))
-            return self.find_direction(path_to_destination, knowledge, bot_pos)
-
         return random.choice(POSSIBLE_ACTIONS)
+
+    def explore_map(self, current_position, knowledge):
+        if self.exploration_goal is None or self.exploration_goal not in self.unseen_coords:
+            self.exploration_goal = random.choice(self.unseen_coords)
+        path_to_destination = self.find_path(current_position, self.exploration_goal)
+        return self.find_direction(path_to_destination, knowledge, current_position)
 
     def praise(self, score: int) -> None:
         pass
