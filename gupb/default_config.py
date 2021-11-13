08c55bcd300c457b0a7d4565a36a395ee03af28b
domo <domo2333@gmail.com>
2021-11-13 14:23:00 +0100

    Add k-bandit algorithm + archer tactic

--- gupb/default_config.py
+++ gupb/default_config.py
@@ -1,5 +1,5 @@
 from gupb.controller import keyboard
-from gupb.controller import random
+from gupb.controller import random, bandyta
 
 
 CONFIGURATION = {
@@ -9,15 +9,15 @@ CONFIGURATION = {
         'fisher_island',
     ],
     'controllers': [
-        keyboard.KeyboardController(),
-        random.RandomController("Alice"),
+        random.RandomController("Bobsd"),
+        bandyta.Bandyta('k'),
         random.RandomController("Bob"),
         random.RandomController("Cecilia"),
         random.RandomController("Darius")
     ],
     'start_balancing': False,
-    'visualise': True,
+    'visualise': False,
     'show_sight': None,
-    'runs_no': 1,
+    'runs_no': 10,
     'profiling_metrics': [],  # possible metrics ['all', 'total', 'avg']
 }
