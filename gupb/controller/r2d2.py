a8db7a80d53eeb4f167cbb4e333bc958682d6a99
mhcenic <71264360+mhcenic@users.noreply.github.com>
2021-11-13 17:33:29 +0100

    r2d2 v3.0

--- gupb/controller/r2d2.py
+++ gupb/controller/r2d2.py
@@ -102,9 +102,7 @@ class R2D2Controller(controller.Controller):
             else:
                 if random.random() < EXPLORE_COEF:
                     return characters.Action.TURN_RIGHT
-                next_step = [self.position.x + self.facing.value[0],
-                             self.position.y + self.facing.value[1]]
-                if self.map[next_step[0], next_step[1]]:
+                if self.map[self.position.x + self.facing.value[0], self.position.y + self.facing.value[1]]:
                     return characters.Action.STEP_FORWARD
                 return characters.Action.TURN_RIGHT
 
