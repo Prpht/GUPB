96ac6f365d888331735a0e82e8c72c5f9f1d678c
MAJERZE <emilia.majerz@comarch.com>
2021-11-13 05:22:49 +0100

    Code refactoring, bugs removal, current policy update

--- gupb/controller/funny/funny_controller.py
+++ gupb/controller/funny/funny_controller.py
@@ -1,23 +1,17 @@
 from gupb import controller
 from gupb.controller.funny.commons import *
 from gupb.controller.funny.pathing import dijkstra, create_path, get_next_move
-
-SAFE_POS = [
-    Coords(6, 6),
-    Coords(6, 12),
-    Coords(12, 6),
-    Coords(12, 12)
-]
+from gupb.model import weapons
+from gupb.model.arenas import Arena
+from gupb.model.characters import CHAMPION_STARTING_HP
+from gupb.model.effects import Mist
 
 
 class FunnyController(controller.Controller):
     START_RUNNING_FROM_MIST = 220
 
     def __init__(self):
-        with open("resources/arenas/isolated_shrine.gupb", "r") as f:
-            self.arena = f.read().split("\n")[:-1]
-
-        self.reset(None)
+        pass
 
     def __eq__(self, other: object) -> bool:
         if isinstance(other, FunnyController):
@@ -34,20 +28,6 @@ class FunnyController(controller.Controller):
         characters.Facing.RIGHT: [(x, y) for x in range(0, 5) for y in range(-2, 3)]
     }
 
-    AXE_RANGE = {
-        characters.Facing.UP: [(0, -1), (-1, -1), (1, -1)],
-        characters.Facing.DOWN: [(0, 1), (-1, 1), (1, 1)],
-        characters.Facing.LEFT: [(-1, 0), (-1, -1), (-1, 1)],
-        characters.Facing.RIGHT: [(1, 0), (1, -1), (1, 1)]
-    }
-
-    SWORD_RANGE = {
-        characters.Facing.UP: [(0, -i) for i in range(1, 4)],
-        characters.Facing.DOWN: [(0, i) for i in range(1, 4)],
-        characters.Facing.LEFT: [(-i, 0) for i in range(1, 4)],
-        characters.Facing.RIGHT: [(i, 0) for i in range(1, 4)]
-    }
-
     def _reset_surrounding_walls(self):
         self.surrounding_walls = {
             characters.Facing.UP: 0,
@@ -57,41 +37,68 @@ class FunnyController(controller.Controller):
         }
 
     def reset(self, arena_description) -> None:
+        arena = Arena.load(arena_description.name)
+        self.terrain = arena.terrain
+        self.menhir_pos = arena.menhir_position #Coords(9, 9)
+        self.weapon_tiles = {coords: WEAPON_CODING[tile.loot.description().name]
+                                 for coords, tile in self.terrain.items() if tile.loot is not None}
+        with open(f"resources/arenas/{arena_description.name}.gupb", "r") as f:
+            self.arena = f.read().split("\n")[:-1]
+        self.safe_pos = SAFE_POS[arena_description.name]
+        self.weapon_priority = WEAPON_PRIORITY[arena_description.name]
+        self.not_menhir_tiles = set()
+        for y in range(len(self.arena)):
+            for x in range(len(self.arena[0])):
+                if self.arena[y][x] == '.':
+                    self.not_menhir_tiles.add((x, y))
+
+        self.misted_tiles = set()
+        self.hp = CHAMPION_STARTING_HP
         self.ep_it = 0
         self.facing = None
-        self.strategy_iter = 0  # 0-find weapon, 1-hide, 3-go to mehnir
+        self.strategy_iter = 0  # 0-find weapon, 1-find menhir, 2-hide, 4-go to menhir
         self._reset_surrounding_walls()
-
-        self.menhir_pos = Coords(9, 9)
 
         self.curr_target = None
         self.path = []
 
-        self.weapon = "A"
+        self.weapon = "K"
         self.opponent_pos = None
         self.weapon_drops = set()
 
     def praise(self, score: int) -> None:
         pass
 
-    def _get_tiles_in_range(self, pos):
-        if self.weapon == "K":
-            return s(pos, self.facing.value)
+    def _get_weaponable_tiles(self, pos, facing, weapon):
+        if weapon == 'K':
+            weaponable_tiles = weapons.Knife.cut_positions(self.terrain, pos, facing)
+        elif weapon == 'S':
+            weaponable_tiles = weapons.Sword.cut_positions(self.terrain, pos, facing)
+        elif weapon == 'B':
+            weaponable_tiles = weapons.Bow.cut_positions(self.terrain, pos, facing)
+        elif weapon == 'M':
+            weaponable_tiles = weapons.Amulet.cut_positions(self.terrain, pos, facing)
         else:
-            return [s(pos, delta) for delta in FunnyController.AXE_RANGE[self.facing]]
+            weaponable_tiles = weapons.Axe.cut_positions(self.terrain, pos, facing)
+        return weaponable_tiles
+
+    def _update_misted_tiles(self, visible_tiles: characters.ChampionKnowledge.visible_tiles):
+        misted_tiles = set(filter(lambda x: visible_tiles[x].effects and Mist in visible_tiles[x].effects,
+                                   visible_tiles))
+        ### zupdatować płytki które są też na pewno zamglone na podst widocznych mgieł
+        self.misted_tiles.update(misted_tiles)
+
+    def _update_menhir_knowledge(self, visible_tiles: characters.ChampionKnowledge.visible_tiles):
+        menhir_tile = list(filter(lambda x: visible_tiles[x].type == 'menhir', visible_tiles))
+        if menhir_tile:
+            self.menhir_pos = menhir_tile[0]
+            self.not_menhir_tiles = set()
+        else:
+            self.not_menhir_tiles -= self.misted_tiles
 
     def _find_weapon(self, pos):
-        y_max = len(self.arena)
-        x_max = len(self.arena[0])
-
-        weapon_types = ['A']
-        weapons = []
-
-        for y in range(y_max):
-            for x in range(x_max):
-                tile = self.arena[y][x]
-                if tile in weapon_types:
-                    weapons.append(Coords(x, y))
+        weapon_types = [weapon for weapon, priority in self.weapon_priority.items() if priority == 5]
+        weapons = list(filter(lambda x: self.weapon_tiles[x] in weapon_types, self.weapon_tiles))
 
         distances, parents = dijkstra(self.arena, pos, self.facing)
         distance_map = {pos: distances[r(pos)] for pos in weapons}
@@ -99,9 +106,17 @@ class FunnyController(controller.Controller):
 
         return create_path(pos, weapon, parents)
 
+    def _find_menhir(self, pos):
+        distances, parents = dijkstra(self.arena, pos, self.facing)
+        distance_map = {pos: distances[r(pos)] for pos in self.not_menhir_tiles}
+        tile = min(distance_map, key=distance_map.get)
+
+        return create_path(pos, tile, parents)
+
     def _hide(self, pos):
-        distances, parents = dijkstra(self.arena, pos, self.facing, self.weapon_drops)
-        distances_map = {hideout: distances[r(hideout)] for hideout in SAFE_POS}
+        #distances, parents = dijkstra(self.arena, pos, self.facing, self.weapon_drops)
+        distances, parents = dijkstra(self.arena, pos, self.facing, self._worse_weapon_tiles())
+        distances_map = {hideout: distances[r(hideout)] for hideout in self.safe_pos}
         hideout = min(distances_map, key=distances_map.get)
 
         return create_path(pos, hideout, parents)
@@ -124,67 +139,116 @@ class FunnyController(controller.Controller):
     def _get_dangerous_fields(self, enemy_pos, knowledge):
         danger_tiles = [enemy_pos]
 
-        enemy_weapon = knowledge.visible_tiles[enemy_pos].character.weapon
-
-        if enemy_weapon == "amulet":
-            danger_tiles.extend([s(enemy_pos, delta) for delta in [(-1, -1), (-1, 1), (1, -1), (1, 1)]])
-        else:
-            for facing in characters.Facing:
-                if enemy_weapon == "knife":
-                    danger_tiles.append(s(enemy_pos, facing.value))
-                elif enemy_weapon == "bow":
-                    danger_tiles.extend([s(enemy_pos,(i * facing.value[0], i * facing.value[1]))
-                                         for i in range(1, 20)])
-                elif enemy_weapon == "sword":
-                    danger_tiles.extend([s(enemy_pos, delta) for delta in FunnyController.SWORD_RANGE[facing]])
-                else:
-                    danger_tiles.extend([s(enemy_pos, delta) for delta in FunnyController.AXE_RANGE[facing]])
-
+        enemy = knowledge.visible_tiles[enemy_pos].character
+        danger_tiles.extend(self._get_weaponable_tiles(enemy_pos, enemy.facing, enemy.weapon))
         return danger_tiles
 
+    def _run_away(self, pos, danger_tiles):
+        return []
+
+    def _worse_weapon_tiles(self):
+        worse_weapon_tiles = set()
+        current_weapon_priority = self.weapon_priority[self.weapon]
+        for tile, weapon in self.weapon_tiles.items():
+            if self.weapon_priority[weapon] < current_weapon_priority:
+                worse_weapon_tiles.add(tile)
+        return worse_weapon_tiles
+
+    def _update_weapons_knowledge(self, tile, weapon_name):
+        code = WEAPON_CODING[weapon_name]
+        self.weapon_tiles[tile] = code
+        self.arena[tile[1]] = self.arena[tile[1]][:tile[0]] + \
+                              code + \
+                              self.arena[tile[1]][tile[0] + 1:]
+
     def _fight(self, pos, danger_tiles):
-        distances, parents = dijkstra(self.arena, pos, self.facing, set(danger_tiles).update(self.weapon_drops))
+        danger_tiles_set = set(danger_tiles)
+        # danger_tiles_set.update(self.weapon_drops)
+        danger_tiles_set.update(self._worse_weapon_tiles())
+        distances, parents = dijkstra(self.arena, pos, self.facing, danger_tiles_set)
         weak_spots = [s(self.opponent_pos, t) for t in
                       [(-1, -1), (-1, 1), (1, -1), (1, 1)]]
-
-        closest_weak_spot = sorted(weak_spots, key=lambda p: distances[r(p)])[0]
-
-        return create_path(pos, closest_weak_spot, parents)
+        weak_spots = list(filter(lambda x: self.arena[x[1]][x[0]] not in ['=', '#'], weak_spots))
+        if not weak_spots:
+            print("no weak spots")
+            return []
+        else:
+            closest_weak_spot = sorted(weak_spots, key=lambda p: distances[r(p)])[0]
+            return create_path(pos, closest_weak_spot, parents)
 
     def _go_to_menhir(self, pos):
-        distances, parents = dijkstra(self.arena, pos, self.facing, self.weapon_drops)
+        #distances, parents = dijkstra(self.arena, pos, self.facing, self.weapon_drops)
+        distances, parents = dijkstra(self.arena, pos, self.facing, self._worse_weapon_tiles())
+        if self.menhir_pos is None:
+            # misted_tiles_coords = [coords for coords, _ in self.misted_tiles]
+            self.menhir_pos = Coords(25, 25)
         return create_path(pos, self.menhir_pos, parents)
 
     def decide(self, knowledge: characters.ChampionKnowledge) -> characters.Action:
+        self_knowledge = knowledge.visible_tiles.get(knowledge.position).character
+        if self.menhir_pos is None and self.strategy_iter > 2:
+            self.strategy_iter = 2
         if self.facing is None:
-            self.facing = knowledge.visible_tiles.get(knowledge.position).character.facing
-
+            self.facing = self_knowledge.facing
+        self.weapon = WEAPON_CODING[self_knowledge.weapon.name]
+        # if self_knowledge.health < self.hp:
+        #     self.hp = self_knowledge.health
+            ### ogarnac kto bije, reakcja
         pos = knowledge.position
 
         self.ep_it += 1
         if self.ep_it > FunnyController.START_RUNNING_FROM_MIST:
-            self.strategy_iter = 3
+            print("change strategy iter to 4")
+            self.strategy_iter = 4
+        visible_tiles = knowledge.visible_tiles
+        # TODO uwaga na to, czasochłonne
+        # self._update_misted_tiles(visible_tiles)
+        for tile in self.weapon_tiles:
+            if tile in visible_tiles and not visible_tiles[tile].character:
+                try:
+                    self._update_weapons_knowledge(tile, visible_tiles[tile].loot.name)
+                except Exception as e:
+                    print(e, "a")
 
-        tiles_in_range = self._get_tiles_in_range(pos)
+        tiles_in_range = self._get_weaponable_tiles(pos, self.facing, self.weapon)
         for tile in tiles_in_range:
             try:
-                character = knowledge.visible_tiles[tile].character
-                if character is not None:
+                tile_knowledge = knowledge.visible_tiles[tile]
+                if tile_knowledge.loot and tile not in self.weapon_tiles:
+                    print("found it!")
+                    self._update_weapons_knowledge(tile, tile_knowledge.loot.name)
+                character = tile_knowledge.character
+                if character is not None: ## dolozyc ze jesli ja nie jestem w jego zasiegu albo jestem ale
+                                            # mam wystarczajaco duzo zycia
+                    print("attack")
                     if character.health <= 2:
+                        try:
+                            self._update_weapons_knowledge(tile, character.weapon.name)
+                            print("added " + character.weapon.name)
+                        except Exception as e:
+                            print(e)
                         self.weapon_drops.add(tile)
                     return characters.Action.ATTACK
             except KeyError:
                 pass
-
+        print(f"Path len: {len(self.path)}, strategy iter: {self.strategy_iter}")
         if len(self.path) == 0:
             if self.strategy_iter == 0:
+                print("Find weapon")
                 self.path = self._find_weapon(pos)
             elif self.strategy_iter == 1:
+                print("Find menhir")
+                self._update_menhir_knowledge(visible_tiles)
+                self.path = self._find_menhir(pos)
+
+            elif self.strategy_iter == 2:
+                print("Hide")
                 self.path = self._hide(pos)
-            elif self.strategy_iter == 3:
+            elif self.strategy_iter == 4:
+                print("Menhir go")
                 self.path = self._go_to_menhir(pos)
 
-        if self.strategy_iter != 2:
+        if self.strategy_iter != 3:
             opposing_characters_pos = []
             for delta in FunnyController.FACING_DELTAS[self.facing]:
                 try:
@@ -200,18 +264,25 @@ class FunnyController(controller.Controller):
 
                 for tile in self.path:
                     if tile in danger_tiles:
+                        print("Fight")
                         self.opponent_pos = opponent
                         self.path = self._fight(pos, danger_tiles)
                         break
 
         if len(self.path) == 0:
+            # if self.strategy_iter < 4 and self.weapon_priority[self.weapon] < 4:
+            #     print("Find weapon cause bad weapon")
+            #     self.path = self._find_weapon(pos)
+            # else:
+            print("Hold pos")
             action = self._hold_pos(knowledge)
         else:
             action = get_next_move(pos, self.facing, self.path[-1])
+            print("action: ", action)
             if action == characters.Action.STEP_FORWARD:
                 self.path.pop()
                 if len(self.path) == 0:
-                    if self.opponent_pos != None:
+                    if self.opponent_pos is not None:
                         self.opponent_pos = None
                     else:
                         self.strategy_iter += 1
