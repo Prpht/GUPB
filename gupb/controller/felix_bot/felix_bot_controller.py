cae9966a49d3c2c54ae9e5046576fb83cba698cd
Agnosic <jan.ilczuk@gmail.com>
2021-11-13 02:46:54 +0100

    Add multiarmed bandit e greedy algorithm for 3 bandits

--- gupb/controller/felix_bot/felix_bot_controller.py
+++ gupb/controller/felix_bot/felix_bot_controller.py
@@ -2,23 +2,13 @@ from .bow_strategy import BowStrategy
 from .sword_strategy import SwordStrategy
 from .axe_strategy import AxeStrategy
 
+from .strategy_rewards_log import StrategyRewardsLog
+
 from gupb import controller
 from gupb.model import arenas
 from gupb.model import characters
 
 import numpy as np
-
-POSSIBLE_ACTIONS = [
-    characters.Action.TURN_LEFT,
-    characters.Action.TURN_RIGHT,
-    characters.Action.STEP_FORWARD,
-]
-
-WEAPON_RANGE = {
-    'bow_loaded': 50,
-    'sword': 3,
-    'knife': 1
-}
 
 
 # noinspection PyUnusedLocal
@@ -26,9 +16,10 @@ WEAPON_RANGE = {
 class FelixBotController(controller.Controller):
     def __init__(self, first_name: str):
         self.first_name: str = first_name
-        self.current_strategy = SwordStrategy()
+        self.current_strategy = None
         self.strategies = [SwordStrategy(), BowStrategy(), AxeStrategy()]
         self.epsilon = 0.04
+        self.rewards_log = StrategyRewardsLog()
 
     def __eq__(self, other: object) -> bool:
         if isinstance(other, FelixBotController):
@@ -39,11 +30,12 @@ class FelixBotController(controller.Controller):
         return hash(self.name)
 
     def praise(self, score: int) -> None:
-        pass
+        self.rewards_log.record_action(self.current_strategy, score)
 
     def reset(self, arena_description: arenas.ArenaDescription) -> None:
         for strategy in self.strategies:
             strategy.reset(arena_description)
+        self.current_strategy = self.choose_strategy()
 
     def decide(self, knowledge: characters.ChampionKnowledge) -> characters.Action:
         return self.current_strategy.decide(knowledge)
@@ -56,8 +48,21 @@ class FelixBotController(controller.Controller):
     def preferred_tabard(self) -> characters.Tabard:
         return characters.Tabard.YELLOW
 
-    # def choose_bandit(self):
+    def choose_strategy(self):
+        if np.random.uniform(0, 1, 1) < self.epsilon:
+            strategy = self.get_random_strategy()
+        else:
+            strategy= self.get_current_best_strategy()
 
+        return strategy
+
+    def get_current_best_strategy(self):
+        estimates = []
+        for strategy in self.strategies:
+            strategy_record = self.rewards_log[strategy]
+            estimates.append(strategy_record['reward'] / strategy_record['actions'])
+
+        return self.strategies[np.argmax(estimates)]
 
     def get_random_strategy(self):
         return np.random.choice(self.strategies)
