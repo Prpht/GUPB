b6c226d07d857e812acb51130d162027fe6cb7f1
Agnosic <jan.ilczuk@gmail.com>
2021-12-31 02:47:37 +0100

    Change safeplaces

--- gupb/controller/felix_bot/strategy.py
+++ gupb/controller/felix_bot/strategy.py
@@ -25,12 +25,9 @@ WEAPON_RANGE = {
 
 SAFE_POS = {
     'fisher_island': [
-        Coords(9, 34),
-        Coords(12, 15),
-        Coords(30, 5),
-        Coords(39, 10),
-        Coords(30, 42),
-        Coords(39, 26)
+        Coords(39, 12),
+        Coords(4, 30),
+        Coords(7, 9)
     ],
     'isolated_shrine': [
         Coords(6, 6),
@@ -39,17 +36,10 @@ SAFE_POS = {
         Coords(12, 12)
     ],
     'archipelago': [
-        Coords(7, 6),
-        Coords(24, 18),
-        Coords(9, 30),
-        Coords(9, 33),
-        Coords(8, 32),
-        Coords(32, 38),
-        Coords(38, 20),
-        Coords(38, 22),
-        Coords(27, 20),
-        Coords(31, 8),
-        Coords(36, 8)
+        Coords(40, 7),
+        Coords(42, 30),
+        Coords(23, 45),
+        Coords(8, 31)
     ],
     'dungeon': [
         Coords(27, 24),
@@ -65,6 +55,14 @@ SAFE_POS = {
         Coords(14, 9),
         Coords(14, 12),
         Coords(10, 12)
+    ],
+    'wasteland': [
+        Coords(13, 25),
+        Coords(13, 27),
+        Coords(33, 36),
+        Coords(5, 6),
+        Coords(5, 43),
+        Coords(46, 44)
     ]
 }
 
