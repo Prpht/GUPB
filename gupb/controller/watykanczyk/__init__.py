09fa165fd1d8ead562b958918990bcddfffd9e70
Adam Ksiezyk <adam.ksiezyk@gmail.com>
2022-10-05 00:47:49 +0200

    Initialize watykanczyk controller

---
+++ gupb/controller/watykanczyk/__init__.py
@@ -0,0 +1,10 @@
+from .controller import WatykanczykController
+
+__all__ = [
+    'WatykanczykController',
+    'POTENTIAL_CONTROLLERS',
+]
+
+POTENTIAL_CONTROLLERS = [
+    WatykanczykController("JP2"),
+]
---
+++ gupb/controller/watykanczyk/controller.py
@@ -0,0 +1,44 @@
+import random
+
+from gupb import controller
+from gupb.model import arenas
+from gupb.model import characters
+
+POSSIBLE_ACTIONS = [
+    characters.Action.TURN_LEFT,
+    characters.Action.TURN_RIGHT,
+    characters.Action.STEP_FORWARD,
+    characters.Action.ATTACK,
+]
+
+
+# noinspection PyUnusedLocal
+# noinspection PyMethodMayBeStatic
+class WatykanczykController(controller.Controller):
+    def __init__(self, first_name: str):
+        self.first_name: str = first_name
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, WatykanczykController):
+            return self.first_name == other.first_name
+        return False
+
+    def __hash__(self) -> int:
+        return hash(self.first_name)
+
+    def decide(self, knowledge: characters.ChampionKnowledge) -> characters.Action:
+        return random.choice(POSSIBLE_ACTIONS)
+
+    def praise(self, score: int) -> None:
+        pass
+
+    def reset(self, arena_description: arenas.ArenaDescription) -> None:
+        pass
+
+    @property
+    def name(self) -> str:
+        return f'WatykanczykController{self.first_name}'
+
+    @property
+    def preferred_tabard(self) -> characters.Tabard:
+        return characters.Tabard.YELLOW
