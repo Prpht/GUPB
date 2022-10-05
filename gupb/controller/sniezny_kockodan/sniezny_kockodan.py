9ea4b80b68ea287ee476fdc889d9f2384e4ad862
Adam Kaczmarczyk <adam8487@interia.pl>
2022-10-05 21:26:27 +0200

    Created SnieznyKockodanController

---
+++ gupb/controller/sniezny_kockodan/sniezny_kockodan.py
@@ -0,0 +1,45 @@
+import random
+
+from gupb import controller
+from gupb.model import arenas
+from gupb.model import characters
+
+POSSIBLE_ACTIONS = [
+    characters.Action.TURN_LEFT,
+    characters.Action.TURN_RIGHT,
+    characters.Action.STEP_FORWARD,
+    characters.Action.ATTACK,
+]
+
+
+# noinspection PyUnusedLocal
+# noinspection PyMethodMayBeStatic
+class SnieznyKockodanController(controller.Controller):
+    def __init__(self, first_name: str):
+        self.first_name: str = first_name
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, SnieznyKockodanController):
+            return self.first_name == other.first_name
+        return False
+
+    def __hash__(self) -> int:
+        return hash(self.first_name)
+
+    def decide(self, knowledge: characters.ChampionKnowledge) -> characters.Action:
+        return random.choice(POSSIBLE_ACTIONS)
+
+    def praise(self, score: int) -> None:
+        pass
+
+    def reset(self, arena_description: arenas.ArenaDescription) -> None:
+        pass
+
+    @property
+    def name(self) -> str:
+        return f'SnieznyKockodanController{self.first_name}'
+
+    @property
+    def preferred_tabard(self) -> characters.Tabard:
+        return characters.Tabard.WHITE
+
