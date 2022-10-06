20204d01b31044a46225a9e6d8ca162db0c0bd4d
Kamil Wierciak <kamil.wierciak99@gmail.com>
2022-10-06 22:35:43 +0200

    Add new class that stores information about arena and provide necessary api to expand the land

--- gupb/controller/dart/dart_controller.py
+++ gupb/controller/dart/dart_controller.py
@@ -1,7 +1,8 @@
 import random
 
 from gupb import controller
-from gupb.model import arenas
+from gupb.controller.dart.arena_knowledge import ArenaKnowledge
+from gupb.model import arenas, coordinates
 from gupb.model import characters
 
 POSSIBLE_ACTIONS = [
@@ -17,6 +18,8 @@ POSSIBLE_ACTIONS = [
 class DartController(controller.Controller):
     def __init__(self, first_name: str):
         self.first_name: str = first_name
+        self.arena_knowledge = None
+        self.path = None
 
     def __eq__(self, other: object) -> bool:
         if isinstance(other, DartController):
@@ -27,8 +30,17 @@ class DartController(controller.Controller):
         return hash(self.first_name)
 
     def decide(self, knowledge: characters.ChampionKnowledge) -> characters.Action:
+        if self.path is None:
+            self.path = self.arena_knowledge.find_path(knowledge.position, coordinates.Coords(15,2))
+        
+        next_position = coordinates.Coords(*self.path[0])
+        desired_facing = self.arena_knowledge.get_desired_facing(knowledge.position, next_position)
+        current_facing = self.arena_knowledge.get_facing(knowledge)
+        desired_action = self.arena_knowledge.determine_action(current_facing, desired_facing)
+        if desired_action == characters.Action.STEP_FORWARD:
+            self.path.pop()
         print("decide!!!")
-        return random.choice(POSSIBLE_ACTIONS)
+        return desired_action
 
     def praise(self, score: int) -> None:
         print("praise!!!!")
@@ -36,6 +48,8 @@ class DartController(controller.Controller):
 
     def reset(self, arena_description: arenas.ArenaDescription) -> None:
         print("reset!!!")
+        self.arena_knowledge = ArenaKnowledge(arena_description)
+
         pass
 
     @property
