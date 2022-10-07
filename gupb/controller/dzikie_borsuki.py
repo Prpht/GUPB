bf8306aa9a2cd72f5be7be3c8cd53d7673259568
HannaTabaszewska <92364769+HannaTabaszewska@users.noreply.github.com>
2022-10-07 22:12:46 +0200

    Controller for the first round

--- gupb/controller/dzikie_borsuki.py
+++ gupb/controller/dzikie_borsuki.py
@@ -5,12 +5,12 @@ from gupb.controller import keyboard
 from gupb.model import arenas
 from gupb.model import characters
 from gupb.model import coordinates
+from gupb.model import arenas
 
 ACTIONS_WITH_WEIGHTS = {
-    characters.Action.TURN_LEFT: 0.2,
-    characters.Action.TURN_RIGHT: 0.2,
-    characters.Action.STEP_FORWARD: 0.5,
-    characters.Action.ATTACK: 0.1,
+    characters.Action.TURN_LEFT: 0.1,
+    characters.Action.TURN_RIGHT: 0.1,
+    characters.Action.STEP_FORWARD: 0.8,
 }
 
 TABARD_ASSIGNMENT = {
@@ -46,6 +46,9 @@ class DzikieBorsuki:
 
     def reset(self, arena_description: arenas.ArenaDescription) -> None:
         pass
+        """self.menhir_coords = None
+        self.weapon = "knife"
+        self.better_weapon_cords = None"""
 
     def calculate_distance(self, self_position: coordinates.Coords, other_position: coordinates.Coords) -> int:
         distance = math.sqrt((self_position.x - other_position.x) ** 2 + (self_position.y - other_position.y) ** 2)
@@ -61,6 +64,12 @@ class DzikieBorsuki:
                     and visible_tiles[visible_position].character.controller_name != self.first_name:
                 nearest_enemy_distance = min(nearest_enemy_distance,
                                              self.calculate_distance(position, visible_position))
+            """if self.menhir_coords is None and visible_tiles[visible_position].type == 'menhir':
+                self.menhir_coords = visible_position
+            if self.weapon == "knife" and self.better_weapon_cords is None \
+                    and visible_tiles[visible_position].loot is not None:
+                if visible_tiles[visible_position].loot.name == "sword":
+                    self.better_weapon_cords = visible_position"""
 
         self_description = visible_tiles[position].character
         if self_description is not None:
@@ -68,8 +77,15 @@ class DzikieBorsuki:
 
             if health >= characters.CHAMPION_STARTING_HP * 0.25 and nearest_enemy_distance == 1:
                 return characters.Action.ATTACK
-
-        # return random action considering probability distribution
+            if visible_tiles[visible_position].type == "menhir":
+                return characters.Action.TURN_RIGHT
+            facing = self_description.facing
+            tile_in_front = facing.value + position
+            type_of_tile = visible_tiles[tile_in_front].type
+            if type_of_tile in ["sea", "wall"]:
+                return random.choice([characters.Action.TURN_RIGHT, characters.Action.TURN_LEFT])
+            elif type_of_tile == "menhir":
+                return characters.Action.STEP_FORWARD
         return random.choices(population=list(ACTIONS_WITH_WEIGHTS.keys()),
                               weights=list(ACTIONS_WITH_WEIGHTS.values()),
                               k=1)[0]
