c683880d328aea40b9e5d0a3bb9d59b726865247
Paweł Kocimski <57032707+kocimski@users.noreply.github.com>
2022-10-08 12:55:50 +0200

    IntercontinentalBajers v1.0 (#130)

---
+++ gupb/controller/intercontinental_bajers/__init__.py
@@ -0,0 +1,11 @@
+
+from .intercontinental_bajers import IntercontinentalBajers
+
+__all__ = [
+    'IntercontinentalBajers',
+    'POTENTIAL_CONTROLLERS'
+]
+
+POTENTIAL_CONTROLLERS = [
+    IntercontinentalBajers("IntercontinentalBajers"),
+]
