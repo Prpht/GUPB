3f376a401eb4eb8fb3ca5ced35c3103a3b8b9579
Weronika Owsianka <57807232+wowsianka@users.noreply.github.com>
2022-10-08 12:34:13 +0200

    Shrek v1.0 (#121)
    * Add Shrek controller
    
    Controller has the abilities to: check if obstacle in front,  randomly make moves (higher prob on wall), attacks if his health is good
    
    * Added extra features to controller
    
    Co-authored-by: Weronika Owsianka <wowsianka@griddynamics.com>

--- gupb/default_config.py
+++ gupb/default_config.py
@@ -1,6 +1,6 @@
 from gupb.controller import keyboard
 from gupb.controller import random
-
+from gupb.controller.shrek import shrek
 
 CONFIGURATION = {
     'arenas': [
@@ -12,6 +12,7 @@ CONFIGURATION = {
         random.RandomController("Bob"),
         random.RandomController("Cecilia"),
         random.RandomController("Darius"),
+        shrek.ShrekController("Fiona")
     ],
     'start_balancing': False,
     'visualise': True,
