3592da2c3487d83cd590983108c2e2f7f35650fe
HannaTabaszewska <92364769+HannaTabaszewska@users.noreply.github.com>
2022-10-12 21:03:42 +0200

    Findings paths and following them to a destination

--- gupb/controller/dzikie_borsuki/dzikie_borsuki.py
+++ gupb/controller/dzikie_borsuki/dzikie_borsuki.py
@@ -8,7 +8,7 @@ from gupb.model import coordinates
 from gupb.model import arenas
 from gupb.model import weapons
 
-from gupb.controller.dzikie_borsuki.utils import PathFinder
+from gupb.controller.dzikie_borsuki import utils
 
 ACTIONS_WITH_WEIGHTS = {
     characters.Action.TURN_LEFT: 0.1,
@@ -67,7 +67,7 @@ class DzikieBorsuki:
 
     def reset(self, arena_description: arenas.ArenaDescription) -> None:
         self.menhir_coords = None
-        self.gps = PathFinder(arena_description)
+        self.gps = utils.PathFinder(arena_description)
         self.path = []
         """self.menhir_coords = None
         self.weapon = "knife"
@@ -90,16 +90,17 @@ class DzikieBorsuki:
                                              self.calculate_distance(position, visible_position))
 
                 print("Wróg w odległości ", nearest_enemy_distance)"""
-        for visible_position in visible_tiles.keys():
-            if self.menhir_coords is None and visible_tiles[visible_position].type == 'menhir':
-                self.menhir_coords = visible_position
+        if self.menhir_coords is None:
+            for visible_position in visible_tiles.keys():
+                if self.menhir_coords is None and visible_tiles[visible_position].type == 'menhir':
+                    self.menhir_coords = visible_position
             """
             if self.weapon == "knife" and self.better_weapon_cords is None \
                     and visible_tiles[visible_position].loot is not None:
                 if visible_tiles[visible_position].loot.name == "sword":
                     self.better_weapon_cords = visible_position """
         self_description = visible_tiles[position].character
-        if self.menhir_coords is not None:
+        if self.menhir_coords is not None and len(self.path) == 0:
             path_to_menhir = self.gps.find_path(position, coordinates.Coords(self.menhir_coords[0],\
                                                                               self.menhir_coords[1]))
             self.path = path_to_menhir
@@ -112,11 +113,19 @@ class DzikieBorsuki:
                 return characters.Action.ATTACK
             if visible_tiles[position].type == "menhir":
                 return characters.Action.TURN_RIGHT
+            if len(self.path) != 0:
+                if position == self.path[0]:
+                    self.path.pop(0)
+                next_action = utils.next_step(position, coordinates.Coords(*self.path[0]), facing)
+                return next_action
+
+            #jeśli napiszemy iście do celu i jakieś startegie odkrywania mapy, to ten kawałek kodu nie będzie potrzebny
             type_of_tile = tile_in_front.type
             if type_of_tile in ["sea", "wall"]:
                 return random.choice([characters.Action.TURN_RIGHT, characters.Action.TURN_LEFT])
             elif type_of_tile == "menhir":
                 return characters.Action.STEP_FORWARD
+
         return random.choices(population=list(ACTIONS_WITH_WEIGHTS.keys()),
                               weights=list(ACTIONS_WITH_WEIGHTS.values()),
                               k=1)[0]
--- gupb/controller/dzikie_borsuki/utils.py
+++ gupb/controller/dzikie_borsuki/utils.py
@@ -1,15 +1,13 @@
 import pathfinding as pth
 import numpy as np
+from typing import List
 
 from gupb.model.coordinates import Coords
 from gupb.model import arenas
+from gupb.model import characters
 from pathfinding.core.diagonal_movement import DiagonalMovement
 from pathfinding.core.grid import Grid
 from pathfinding.finder.a_star import AStarFinder
-
-
-def say_hi() -> None:
-    print("hi")
 
 class PathFinder():
     def __init__(self, arena_description: arenas.ArenaDescription):
@@ -17,16 +15,29 @@ class PathFinder():
         self.arena_matrix = self.create_arena_matrix()
 
     def create_arena_matrix(self):
-        matrix = np.zeros(self.arena.size)
+        matrix = [[0 for x in range(self.arena.size[0])] for y in range(self.arena.size[1])]
         for coords, tile in self.arena.terrain.items():
             if tile.description().type in ["land", "menhir"]:
-                matrix[coords] = 1
+                matrix[coords[1]][coords[0]] = 1
+            else:
+                matrix[coords[1]][coords[0]] = 0
         return matrix
 
-    def find_path(self, current_position: Coords, destination: Coords) -> [Coords]:
+
+
+
+    def find_path(self, current_position: Coords, destination: Coords) -> List[Coords]:
         grid = Grid(matrix=self.arena_matrix)
-        start = grid.node(current_position.x, current_position.y)
-        end = grid.node(destination.x, destination.y)
+        start = grid.node(current_position[0], current_position[1])
+        end = grid.node(destination[0], destination[1])
         finder = AStarFinder(diagonal_movement=DiagonalMovement.never)
-        path = finder.find_path(start,end, grid)[0]
-        return path
+        path, _ = finder.find_path(start,end, grid)
+        return path[1:]
+
+def next_step(current_position: Coords, next_position: Coords, facing) -> characters.Action:
+    print("stoi ", current_position, ", idzie do ", next_position)
+    if (current_position + facing.value == next_position):
+        return characters.Action.STEP_FORWARD
+    if (current_position + facing.turn_left().value == next_position):
+        return characters.Action.TURN_LEFT
+    return characters.Action.TURN_RIGHT
