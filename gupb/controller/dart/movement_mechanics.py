0adac5eac62d52ad79b1f16508acfc104eb3893d
Kamil Wierciak <56774884+FEJTWOW@users.noreply.github.com>
2022-10-15 13:41:50 +0200

    Dart [v2.0] (#140)
    * Initialize watykanczyk controller
    
    * Rename watykanczyk to dart, add debug prints
    
    * Add new class that stores information about arena and provide necessary api to expand the land
    
    * Fix walking and pathfinding
    
    * Refactor code and extract strategy
    
    * Find middlepoint
    
    * Handle colision
    
    * Attack when opponent is in front
    
    * Fix pathfinding multiple usages
    
    * Execute reset in praise
    
    * Fix oponent in front
    
    * Add axe strategy
    
    * Refactor, extract State and PathFinder
    
    * Add default strategy
    
    * Handle thrown exceptions
    
    * Add can attack opponent functions
    
    * -> Add informations about weapons
    -> Add new methods to find the shortest path to the closest desired weapon
    
    * Refactor code
    
    * Add fixed menhir coords to middle coords
    
    * Add attack related functions
    
    * Add attact actions to strategy
    
    * Fix weapon methods for different bow names
    
    * Add run away action
    
    * Add simple strategy
    
    * Add sword to possible weapons, fix issue with finding middle coords
    
    * Change behaviour after founding weapon
    
    Co-authored-by: Adam Ksiezyk <adam.ksiezyk@gmail.com>

--- gupb/controller/dart/movement_mechanics.py
+++ gupb/controller/dart/movement_mechanics.py
@@ -1,11 +1,13 @@
-from typing import Dict, List
+import math
+from typing import Dict, List, Optional
 from pathfinding.core.grid import Grid
 from pathfinding.core.diagonal_movement import DiagonalMovement
 from pathfinding.finder.a_star import AStarFinder
-from gupb.model.arenas import Arena, ArenaDescription
+from gupb.model.arenas import FIXED_MENHIRS, Arena, ArenaDescription
 from gupb.model.coordinates import Coords
-from gupb.model.characters import Action, ChampionKnowledge, Facing
-from gupb.model.tiles import TileDescription
+from gupb.model.characters import Action, ChampionDescription, ChampionKnowledge, Facing
+from gupb.model.tiles import Tile, TileDescription
+from gupb.model.weapons import Amulet, Axe, Bow, Knife, Sword, Weapon
 
 ArenaMatrix = List[List[bool]]
 
@@ -25,10 +27,13 @@ TURN_ACTIONS = {
 }
 
 
-class PathFinder():
+class MapKnowledge():
     def __init__(self, arena_description: ArenaDescription):
         self.arena = Arena.load(arena_description.name)
         self.arena_matrix = self._create_arena_matrix()
+        self.arena_menhir = None
+        self.weapons: Dict[Coords, str] = dict()
+        self.initialize_weapons_positions()
 
     def _create_arena_matrix(self) -> ArenaMatrix:
         arena_matrix = [[1 for _ in range(self.arena.size[0])] for _ in range(self.arena.size[1])]
@@ -37,6 +42,8 @@ class PathFinder():
         return arena_matrix
 
     def find_middle_cords(self) -> Coords:
+        if self.arena.name in FIXED_MENHIRS:
+            return FIXED_MENHIRS[self.arena.name]
         y = self.arena.size[0]//2
         for i in range(self.arena.size[0]//2):
             x = self.arena.size[0]//2 + i
@@ -51,29 +58,64 @@ class PathFinder():
         path, _ = finder.find_path(start, end, grid)
         return path[1:]
 
+    def initialize_weapons_positions(self) -> None:
+        self.weapons = {coords: tile.loot.description().name
+                        for coords, tile in self.arena.terrain.items() if tile.loot is not None}
 
-def find_shortest_path(*paths: List[Coords]) -> List[Coords]:
-    return min(*paths, key=lambda p: len(p))
+    def update_weapons_positions(self) -> None:
+        for coords in self.weapons:
+            self.weapons[coords] = self.arena.terrain[coords].loot.description().name
+
+    def get_closest_weapon_path(self, current_position: Coords, *weapon_types: str) -> List[Coords]:
+        weapons_coords = [coords for coords, type in self.weapons.items() if type.startswith(weapon_types)]
+        return self.find_shortest_path(current_position, weapons_coords)
+
+    def find_shortest_path(self, current_position: Coords, destinations: List[Coords]) -> List[Coords]:
+        paths = [self.find_path(current_position, dest) for dest in destinations]
+        return min(paths, key=lambda p: len(p))
+
+    def find_closest_coords(self, current_position: Coords, destinations: List[Coords]) -> Coords:
+        return min(destinations, key=lambda dest: len(self.find_path(current_position, dest)))
+
+    def can_attack(self, knowledge: ChampionKnowledge, opponent_position: Coords) -> bool:
+        weapon = get_weapon(get_champion_weapon(knowledge))
+        facing = get_facing(knowledge)
+        weapon_cut_positions = weapon.cut_positions(self.arena.terrain, knowledge.position, facing)
+        return opponent_position in weapon_cut_positions
+
+    def get_facing_for_attack(self, knowledge: ChampionKnowledge, opponent_position: Coords) -> Optional[Facing]:
+        for facing in Facing:
+            if self.can_attack(knowledge, opponent_position):
+                return facing
+        return None
+
+    def is_land(self, coords: Coords) -> bool:
+        return self.arena_matrix[coords.y][coords.x]
+
+    def find_mist_coords(self, knowledge: ChampionKnowledge) -> Optional[Coords]:
+        mist_coords = [Coords(*coords) for coords, tile in knowledge.visible_tiles.items() if is_mist(tile)]
+        mist_coords_and_distances = [(coords, euclidean_distance(knowledge.position, coords)) for coords in mist_coords]
+        return min(mist_coords_and_distances, key=lambda x: x[1])[0] if mist_coords_and_distances else None
 
 
 def follow_path(path: List[Coords], knowledge: ChampionKnowledge) -> Action:
     next_position = Coords(*path[0])
-    current_facing = _get_facing(knowledge)
-    desired_facing = _get_desired_facing(knowledge.position, next_position)
-    return _determine_action(current_facing, desired_facing)
+    current_facing = get_facing(knowledge)
+    desired_facing = get_desired_facing(knowledge.position, next_position)
+    return determine_action(current_facing, desired_facing)
 
 
-def _get_facing(knowledge: ChampionKnowledge) -> Facing:
+def get_facing(knowledge: ChampionKnowledge) -> Facing:
     tile = knowledge.visible_tiles.get(knowledge.position)
     return tile.character.facing
 
 
-def _get_desired_facing(current_position: Coords, desired_position: Coords) -> Facing:
+def get_desired_facing(current_position: Coords, desired_position: Coords) -> Facing:
     desired_facing_coordinates = desired_position - current_position
     return Facing(desired_facing_coordinates)
 
 
-def _determine_action(current_facing: Facing, desired_facing: Facing) -> Action:
+def determine_action(current_facing: Facing, desired_facing: Facing) -> Action:
     if current_facing == desired_facing:
         return Action.STEP_FORWARD
     return TURN_ACTIONS[(current_facing, desired_facing)]
@@ -83,4 +125,37 @@ def is_opponent_in_front(opponent_position: Coords, visible_tiles: Dict[Coords, 
     if opponent_position not in visible_tiles:
         return False
     opponent = visible_tiles[opponent_position].character
-    return opponent is not None
+    return is_opponent(opponent)
+
+
+def is_opponent(character: ChampionDescription) -> bool:
+    return not (character is None or character.controller_name.startswith("DartController"))
+
+
+def is_mist(tile: Tile) -> bool:
+    return "mist" in [e.type for e in tile.effects]
+
+
+def get_champion_weapon(knowledge: ChampionKnowledge) -> str:
+    return knowledge.visible_tiles[knowledge.position].character.weapon.name
+
+
+def get_weapon(weapon_name: str) -> Weapon:
+    if weapon_name == "knife":
+        return Knife()
+    if weapon_name == "sword":
+        return Sword()
+    if weapon_name.startswith("bow"):
+        return Bow()
+    if weapon_name == "axe":
+        return Axe()
+    if weapon_name == "amulet":
+        return Amulet()
+
+
+def find_opponents(visible_tiles: Dict[tuple, TileDescription]) -> Dict[Coords, str]:
+    return {Coords(*coords): tile.character.controller_name for coords, tile in visible_tiles.items() if is_opponent(tile.character)}
+
+
+def euclidean_distance(c1: Coords, c2: Coords) -> float:
+    return math.sqrt((c1.x - c2.x)**2 + (c1.y - c2.y)**2)
