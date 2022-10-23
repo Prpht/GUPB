e4d5a59cd0364bb5d3ebb0e37a9f11bc09b5a889
HannaTabaszewska <92364769+HannaTabaszewska@users.noreply.github.com>
2022-10-23 21:48:17 +0200

    U mnie działa :') [Borsuki v3.1] (#161)

--- gupb/controller/dzikie_borsuki/dzikie_borsuki.py
+++ gupb/controller/dzikie_borsuki/dzikie_borsuki.py
@@ -79,43 +79,43 @@ class DzikieBorsuki:
             # Szukamy menhira
             if self.menhir_coords is None and visible_tiles[visible_position].type == 'menhir':
                 self.menhir_coords = visible_position
-                
+
             # Szukamy lepszej broni
             if visible_tiles[visible_position].loot is not None:
                 my_weapon = WEAPON_RANKING[knowledge.visible_tiles[knowledge.position].character.weapon.name]
                 other_weapon = WEAPON_RANKING[visible_tiles[visible_position].loot.name]
                 if other_weapon > my_weapon:
                     self.better_weapon_coords = visible_position
-                    
-            # Jesli podnieslismy lepsza bron to juz nas nie interesuje 
+
+            # Jesli podnieslismy lepsza bron to juz nas nie interesuje
             if self.better_weapon_coords == position:
                 self.better_weapon_coords = None
-                
+
         self_description = visible_tiles[position].character
         # Jesli menhir jest znaleziony to idziemy do niego
         if self.menhir_coords is not None and len(self.path) == 0:
-            path_to_menhir = self.gps.find_path(position, coordinates.Coords(self.menhir_coords[0],\
-                                                                              self.menhir_coords[1]))
+            path_to_menhir = self.gps.find_path(position, coordinates.Coords(self.menhir_coords[0], \
+                                                                             self.menhir_coords[1]))
             self.path = path_to_menhir
-        
+
         # Ale jesli widzimy lepsza bron to idziemy po nia najpierw
         if self.better_weapon_coords is not None and len(self.path) == 0:
-            path_to_weapon = self.gps.find_path(position, coordinates.Coords(self.better_weapon_coords[0],\
+            path_to_weapon = self.gps.find_path(position, coordinates.Coords(self.better_weapon_coords[0], \
                                                                              self.better_weapon_coords[1]))
             self.path = path_to_weapon
-            
+
         if self_description is not None:
             health = self_description.health
             facing = self_description.facing
             tile_in_front_coords = facing.value + position
             tile_in_front = visible_tiles[tile_in_front_coords]
             weaponable_tiles = utils.get_weaponable_tiles(self.arena, position, facing, self.weapon)
-            
+
             if health >= characters.CHAMPION_STARTING_HP * 0.25:
                 for tile_coords in weaponable_tiles:
                     if visible_tiles[tile_coords].character is not None:
                         return characters.Action.ATTACK
-            
+
             if visible_tiles[position].type == "menhir":
                 return characters.Action.TURN_RIGHT
             if len(self.path) != 0:
@@ -124,7 +124,7 @@ class DzikieBorsuki:
                 next_action = utils.next_step(position, coordinates.Coords(*self.path[0]), facing)
                 return next_action
 
-            #jeśli napiszemy iście do celu i jakieś startegie odkrywania mapy, to ten kawałek kodu nie będzie potrzebny
+            # jeśli napiszemy iście do celu i jakieś startegie odkrywania mapy, to ten kawałek kodu nie będzie potrzebny
             type_of_tile = tile_in_front.type
             if type_of_tile in ["sea", "wall"]:
                 return random.choice([characters.Action.TURN_RIGHT, characters.Action.TURN_LEFT])
