68a959349df91ff8b17d921f63e5f1b9053b1076
Michael <michalsobieraj966@wp.pl>
2022-11-04 01:27:12 +0100

    Path finding with BFS

--- gupb/controller/killer/killer.py
+++ gupb/controller/killer/killer.py
@@ -1,5 +1,3 @@
-from typing import List
-from math import acos
 import numpy as np
 from enum import Enum
 from gupb import controller
@@ -7,17 +5,10 @@ from gupb.model import arenas
 from gupb.model import characters
 from gupb.model.characters import Action, Facing
 from gupb.model.coordinates import Coords, add_coords
+from .utils import KillerInterest, PathConstants, find_paths, path_to_actions
+
 import logging
-
-from .utils import PathConstants, find_path
 verbose_logger = logging.getLogger("verbose")
-
-
-class KillerInterest(Enum):
-    POINT_ON_MAP = 2
-    ITEM = 3
-    KILLING = 4
-    MENHIR = 5
 
 
 class KillerAction(Enum):
@@ -51,13 +42,13 @@ class KillerController(controller.Controller):
         for coord, tile in knowledge.visible_tiles.items():
 
             if tile.type == "land":
-                self.game_map[coord[0], coord[1]] = PathConstants.WALKABLE.value
+                self.game_map[coord[1], coord[0]] = PathConstants.WALKABLE.value
                 if tile.character is not None:
-                    self.game_map[coord[0], coord[1]] = KillerInterest.KILLING.value
+                    self.game_map[coord[1], coord[0]] = KillerInterest.KILLING.value
 
             if self.menhir_pos is None:
                 if tile.type == "menhir":
-                    self.game_map[coord[0], coord[1]] = KillerInterest.MENHIR.value
+                    self.game_map[coord[1], coord[0]] = KillerInterest.MENHIR.value
                     self.menhir_pos = coord[0], coord[1]
 
     def update_map(self,
@@ -65,41 +56,6 @@ class KillerController(controller.Controller):
                    current_interest: KillerInterest) -> None:
         # to do: Update only player locations (not the whole map)
         pass
-
-    def __get_path_actions(self,
-                           position: Coords,
-                           direction: Coords,
-                           interest: KillerInterest) -> List[Action]:
-        found_path = []
-        is_found = find_path(arr=self.game_map,
-                             visited=np.zeros_like(self.game_map),
-                             curr_pos=position,
-                             path=found_path,
-                             min_sought_val=interest.value)
-        verbose_logger.debug(f"Is path found: {is_found}")
-        actions = []
-        if not is_found:
-            # verbose_logger.debug(f"Map: {self.game_map}")
-            return actions
-        # verbose_logger.debug(f"Found_path: {found_path}")
-
-        curr_d = direction
-        for i in range(len(found_path) - 1):
-            d = found_path[i + 1][0] - found_path[i][0], found_path[i + 1][1] - found_path[i][1]
-            sign = curr_d[0] * d[1] - d[0] * curr_d[1]
-            angle = acos(d[0] * curr_d[0] + d[1] * curr_d[1])
-            if 1. <= angle <= 2.5:
-                if sign < 0:
-                    actions.append(Action.TURN_RIGHT)
-                else:
-                    actions.append(Action.TURN_LEFT)
-            if 2.5 <= angle:
-                actions.append(Action.TURN_RIGHT)
-                actions.append(Action.TURN_RIGHT)
-            actions.append(Action.STEP_FORWARD)
-            curr_d = d
-        # verbose_logger.debug(actions)
-        return actions
 
     def find_new_interest(self, curr_position):
         x_max, y_max = self.game_map.shape
@@ -133,36 +89,42 @@ class KillerController(controller.Controller):
         if distances:
             self.game_map[sorted(distances, key=lambda x: x[1])[-1][0]] = 2
 
-
-
     @staticmethod
     def get_facing(knowledge: characters.ChampionKnowledge):
         return knowledge.visible_tiles[knowledge.position].character.facing
 
     @staticmethod
     def get_facing_element(knowledge: characters.ChampionKnowledge):
-        return knowledge.visible_tiles[add_coords(knowledge.position, KillerController.get_facing(knowledge).value)]
+        return knowledge.visible_tiles[add_coords(knowledge.position,
+                                                  KillerController.get_facing(knowledge).value)]
 
-    def find(self, knowledge: characters.ChampionKnowledge, interest: KillerInterest):
-        # verbose_logger.debug("Looking for path...")
+    def find_path(self, knowledge: characters.ChampionKnowledge, interest: KillerInterest = None):
+        # Path finding
         current_position = knowledge.position
-        # verbose_logger.debug(f"Curr position: {current_position}")
+        paths = find_paths(arr=self.game_map, curr_pos=current_position)
+        if interest is not None:
+            found_path = paths.get_best_path(val_from=interest.value,
+                                             val_to=interest.value)
+        else:
+            found_path = paths.get_best_path(val_from=KillerInterest.POINT_ON_MAP.value,
+                                             val_to=KillerInterest.MENHIR.value)
+
+        verbose_logger.debug(f"FOUND PATH: {found_path}")
+        # Getting actions for found path
         facing = self.get_facing(knowledge)
-        # verbose_logger.debug(f"Facing: {facing}")
         if facing == Facing.UP:
-            curr_direction = -1, 0
-        elif facing == Facing.DOWN:
-            curr_direction = 1, 0
-        elif facing == Facing.LEFT:
-            curr_direction = 0, -1
-        else:  # facing == Facing.RIGHT
+            #curr_direction = -1, 0
             curr_direction = 0, 1
-        # verbose_logger.debug(f"Current direction: {curr_direction}")
-        path_actions = self.__get_path_actions(current_position, curr_direction, interest)
-        # logging.debug(f"Actions to get there: {path_actions}")
-        if len(path_actions) != 0:
-            self.planned_actions += path_actions
-            self.planned_actions.append(KillerAction.LOOK_AROUND)
+        elif facing == Facing.DOWN:
+            #curr_direction = 1, 0
+            curr_direction = 0, -1
+        elif facing == Facing.LEFT:
+            #curr_direction = 0, -1
+            curr_direction = -1, 0
+        else:  # facing == Facing.RIGHT
+            curr_direction = 1, 0
+        actions = path_to_actions(initial_direction=curr_direction, path=found_path)
+        self.planned_actions += actions
 
     def execute_action(self, action: KillerAction, knowledge: characters.ChampionKnowledge):
         if action == KillerAction.LEARN_MAP:
@@ -175,23 +137,23 @@ class KillerController(controller.Controller):
             self.planned_actions += 4 * [Action.TURN_RIGHT]
 
         if action == KillerAction.FIND_VICTIM:
-            self.find(knowledge, interest=KillerInterest.KILLING)
+            self.find_path(knowledge, interest=KillerInterest.KILLING)
 
         if action == KillerAction.FIND_MENHIR:
-            self.find(knowledge, interest=KillerInterest.MENHIR)
+            self.find_path(knowledge, interest=KillerInterest.MENHIR)
 
         if action == KillerAction.DISCOVER:
             self.find_new_interest(curr_position=knowledge.position)
-            self.find(knowledge, interest=KillerInterest.POINT_ON_MAP)
+            self.find_path(knowledge, interest=KillerInterest.POINT_ON_MAP)
 
-    def check_for_enemies(self, knowledge: characters.ChampionKnowledge):
-        #mark enemies
-        are_enemies = False
-        for coords, value in knowledge.visible_tiles.items():
-            if value.character is not None and value.character.controller_name != 'Killer':
-                self.game_map[coords] = KillerInterest.KILLING.value
-                are_enemies = True
-        return are_enemies
+    # def check_for_enemies(self, knowledge: characters.ChampionKnowledge):
+    #     #mark enemies
+    #     are_enemies = False
+    #     for coords, value in knowledge.visible_tiles.items():
+    #         if value.character is not None and value.character.controller_name != 'Killer':
+    #             self.game_map[coords] = KillerInterest.KILLING.value
+    #             are_enemies = True
+    #     return are_enemies
 
     def decide(self, knowledge: characters.ChampionKnowledge) -> characters.Action:
         facing_element = self.get_facing_element(knowledge)
@@ -205,26 +167,20 @@ class KillerController(controller.Controller):
         if self.menhir_pos == knowledge.position:
             self.execute_action(KillerAction.SPIN_AROUND, knowledge)
 
-        # verbose_logger.debug(f"Planned actions: {self.planned_actions}")
-        if len(self.planned_actions) == 0 and (self.menhir_pos is not None):
-            self.execute_action(KillerAction.FIND_MENHIR, knowledge)
+        self.learn_map(knowledge)
 
-        if len(self.planned_actions) == 0 and self.check_for_enemies(knowledge):  # Could not find menhir
-            self.execute_action(KillerAction.FIND_VICTIM, knowledge)
-
-        if len(self.planned_actions) == 0:  # Could not find victim
-            self.execute_action(KillerAction.DISCOVER, knowledge)
-
-        # verbose_logger.debug(self.planned_actions)
+        if len(self.planned_actions) == 0:
+            verbose_logger.debug("We need to find path")
+            self.find_path(knowledge)
+        else:
+            verbose_logger.debug("No need for path finding")
+        verbose_logger.debug(f"CURRENT POS: {knowledge.position}")
         while len(self.planned_actions) > 0:
             action = self.planned_actions.pop(0)
             if isinstance(action, KillerAction):
                 self.execute_action(action, knowledge)
             else:
-                if action == Action.STEP_FORWARD and facing_element.type != 'land':
-                    return Action.TURN_RIGHT
-                else:
-                    return action
+                return action
         else:
             return Action.TURN_RIGHT
 
--- gupb/controller/killer/utils.py
+++ gupb/controller/killer/utils.py
@@ -1,5 +1,9 @@
 from enum import Enum
-from numpy import ndarray, shape
+from math import acos
+from typing import List, Tuple
+import numpy as np
+from gupb.model.characters import Action
+from .structures import Tree
 
 # Available directions
 _DIR = [[0, 1], [0, -1], [1, 0], [-1, 0]]
@@ -11,32 +15,58 @@ class PathConstants(Enum):
     WALKABLE = 1
 
 
-def find_path(arr: ndarray,
-              visited: ndarray,
-              curr_pos: tuple,
-              path: list,
-              min_sought_val: int = 2
-              ) -> bool:
-    x_bound, y_bound = shape(arr)
+class KillerInterest(Enum):
+    POINT_ON_MAP = 2
+    ITEM = 3
+    KILLING = 4
+    MENHIR = 5
+
+
+def find_paths(arr: np.ndarray, curr_pos: tuple) -> Tree:
+    x_bound, y_bound = np.shape(arr)
+    visited = np.zeros_like(arr)
 
     visited[curr_pos[0]][curr_pos[1]] = 1
-    if arr[curr_pos[0]][curr_pos[1]] == PathConstants.WALL.value:
-        return False
+    queue = [curr_pos]
+    paths = Tree(root_node=curr_pos)
 
-    path.append(curr_pos)
-    if arr[curr_pos[0]][curr_pos[1]] >= min_sought_val:
-        return True
+    while len(queue) > 0:
+        node = queue.pop(0)
+        for i in range(4):
+            x = node[0] + _DIR[i][0]
+            y = node[1] + _DIR[i][1]
 
-    for i in range(4):
-        x = curr_pos[0] + _DIR[i][0]
-        y = curr_pos[1] + _DIR[i][1]
+            if (x >= 0 and y >= 0) and \
+                    (x < x_bound and y < y_bound) and \
+                    (not visited[x][y]):
 
-        if (x >= 0 and y >= 0) and \
-           (x < x_bound and y < y_bound) and \
-           (not visited[x][y]):
-            if find_path(arr, visited, (x, y), path, min_sought_val):
-                return True
+                visited[x][y] = 1
+                pos_value = arr[x][y]
+                if pos_value >= PathConstants.WALKABLE.value:
+                    queue.append((x, y))
+                    paths.append(node_from=node, node_to=(x, y))
+                    if pos_value >= KillerInterest.POINT_ON_MAP.value:
+                        paths.mark_node(node=(x, y), value=pos_value)
+    return paths
 
-    path.pop()
-    return False
 
+def path_to_actions(initial_direction: tuple, path: List[Tuple]) -> List[Action]:
+    actions = []
+    curr_d = initial_direction
+    for i in range(len(path) - 1):
+        d = path[i + 1][0] - path[i][0], path[i + 1][1] - path[i][1]
+        sign = d[1] * curr_d[0] - d[0] * curr_d[1]
+        angle = acos(d[0] * curr_d[0] + d[1] * curr_d[1])
+        if 1. <= angle <= 2.5:
+            if sign < 0:
+                actions.append(Action.TURN_RIGHT)
+            else:
+                actions.append(Action.TURN_LEFT)
+        if 2.5 <= angle:
+            print(d)
+            print(curr_d)
+            actions.append(Action.TURN_RIGHT)
+            actions.append(Action.TURN_RIGHT)
+        actions.append(Action.STEP_FORWARD)
+        curr_d = d
+    return actions
