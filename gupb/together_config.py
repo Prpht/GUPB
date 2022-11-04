9a1c7276772cd3d7e8e0f577ced42763037399e3
Adam Ksiezyk <adam.ksiezyk@gmail.com>
2022-11-04 22:12:02 +0100

    Revert config to upstream

--- gupb/together_config.py
+++ gupb/together_config.py
@@ -14,7 +14,7 @@ from gupb.controller import tuptus
 from gupb.scripts import arena_generator
 
 CONFIGURATION = {
-    'arenas': arena_generator.generate_arenas(1),
+    'arenas': arena_generator.generate_arenas(30),
     'controllers': [
         aksolotl.AksolotlController("Old"),
         aleph_aleph_zero.AlephAlephZeroBot("AA0"),
@@ -31,7 +31,7 @@ CONFIGURATION = {
         tuptus.TuptusController("CiCik"),
     ],
     'start_balancing': False,
-    'visualise': dart.DartController("Dart"),
+    'visualise': False,
     'show_sight': None,
-    'runs_no': 1,
+    'runs_no': 1000,
 }
