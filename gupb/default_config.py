3afeef003a764a701c8cd2978d741fce077d0965
krvwv <pkarwo@wp.pl>
2022-11-05 03:58:39 +0100

    pr fix

--- gupb/default_config.py
+++ gupb/default_config.py
@@ -1,26 +1,17 @@
 from gupb.controller import keyboard
 from gupb.controller import random
-from gupb.controller.sniezny_kockodan import kockodan
+
 
 CONFIGURATION = {
     'arenas': [
-        'lone_sanctum',
+        'fisher_island',
     ],
     'controllers': [
-        #keyboard.KeyboardController(),
+        keyboard.KeyboardController(),
         random.RandomController("Alice"),
         random.RandomController("Bob"),
         random.RandomController("Cecilia"),
         random.RandomController("Darius"),
-        kockodan.SnieznyKockodanController('Kocek'),
-        random.RandomController("Alice"),
-        random.RandomController("Bob"),
-        random.RandomController("Cecilia"),
-        random.RandomController("Darius"),
-        random.RandomController("Alice"),
-        random.RandomController("Bob"),
-        random.RandomController("Cecilia"),
-        random.RandomController("Darius")
     ],
     'start_balancing': False,
     'visualise': True,
@@ -28,4 +19,3 @@ CONFIGURATION = {
     'runs_no': 5,
     'profiling_metrics': [],
 }
-
