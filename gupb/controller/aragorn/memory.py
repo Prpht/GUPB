0b4efdb5fe8fd80da4462b0f94dd4e54b0885c42
Pawel Hanusik <39835401+pawelhanusik@users.noreply.github.com>
2023-10-21 17:45:39 +0200

    Update memory each turn

--- gupb/controller/aragorn/memory.py
+++ gupb/controller/aragorn/memory.py
@@ -1,8 +1,8 @@
 import os
-from typing import Dict, NamedTuple, Optional
+from typing import Dict, NamedTuple, Optional, List
 
 from gupb.model import arenas, tiles, coordinates, weapons
-from gupb.model import characters
+from gupb.model import characters, consumables, effects
 
 
 
@@ -20,6 +20,44 @@ class Memory:
     def update(self, knowledge: characters.ChampionKnowledge) -> None:
         self.position = knowledge.position
         self.no_of_champions_alive = knowledge.no_of_champions_alive
+
+        for coords in knowledge.visible_tiles:
+            visible_tile_description = knowledge.visible_tiles[coords]
+            self.map.terrain[coords].loot = self.weaponDescriptionConverter(visible_tile_description.loot)
+            self.map.terrain[coords].character = visible_tile_description.character
+            self.map.terrain[coords].consumable = self.consumableDescriptionConverter(visible_tile_description.consumable)
+            self.map.terrain[coords].effects = self.effectsDescriptionConverter(visible_tile_description.effects)
+    
+    def weaponDescriptionConverter(self, weaponName: str) -> weapons.Weapon:
+        if weaponName == 'knife':
+            return weapons.Knife
+        elif weaponName == 'sword':
+            return weapons.Sword
+        elif weaponName == 'axe':
+            return weapons.Axe
+        elif weaponName == 'bow':
+            return weapons.Bow
+        elif weaponName == 'amulet':
+            return weapons.Amulet
+        else:
+            return None
+    
+    def consumableDescriptionConverter(self, consumableName: str) -> consumables.Consumable:
+        if consumableName == 'potion':
+            return consumables.Potion
+        return None
+    
+    def effectsDescriptionConverter(self, effects: List[effects.EffectDescription]) -> list[effects.Effect]:
+        convertedEffects = []
+
+        for effect in effects:
+            if effect == 'mist':
+                convertedEffects.append(effects.Mist)
+            # we dont need to know weapon cuts
+            # elif effect == 'weaponcut':
+            #     convertedEffects.append(effects.WeaponCut)
+        
+        return convertedEffects
 
 class Map:
     def __init__(self, name: str, terrain: arenas.Terrain) -> None:
