f00ea9fafc517ac1e55d7d28fb3fd788102a7263
ignsz <kaignacy@gmail.com>
2023-10-21 17:32:21 +0200

    add looking for weapons add looking for mist add looking for menhir

--- gupb/controller/our.py
+++ gupb/controller/our.py
@@ -1,5 +1,5 @@
 import random
-from typing import Dict, Tuple, Optional, List
+from typing import Dict, Tuple, Optional, List, NamedTuple
 
 from pathfinding.core.grid import Grid
 from pathfinding.core.node import GridNode
@@ -19,17 +19,27 @@ POSSIBLE_ACTIONS = [
 
 MAX_SIZE = 20
 
+
+class SeenWeapon(NamedTuple):
+    name: str
+    seen_epoch_nr: int
+
+
 # noinspection PyUnusedLocal
 # noinspection PyMethodMayBeStatic
 class OurController(controller.Controller):
     def __init__(self, first_name: str):
         self.first_name: str = first_name
+        self.current_position = Optional[coordinates.Coords] = None
         # remembering map
         self.epoch: int = 0
         self.seen_tiles: Dict[coordinates.Coords, Tuple[tiles.TileDescription, int]] = {}
 
         # pathfinding
         self.grid: Optional[Grid] = None
+
+        self.menhir_chords: Optional[coordinates.Coords] = None
+        self.wapons_chords: Dict[coordinates.Coords, SeenWeapon] = {}
 
         self.paths = {
             'to_menhir': None,
@@ -48,11 +58,9 @@ class OurController(controller.Controller):
 
     def decide(self, knowledge: characters.ChampionKnowledge) -> characters.Action:
         try:
-            self.seen_tiles.update(dict((x[0], (x[1], self.epoch))for x in knowledge.visible_tiles.items()))
-            self.epoch += 1
-            self.build_grid()
-            self.finder = AStarFinder()
-            x, y = knowledge.position
+            self.update_state(self, knowledge)
+
+            x, y = self.current_position
             path = self.finder.find_path(self.grid.node(x, y), self.grid.node(6, 4), self.grid)
             if not self.actions:
                 self.actions = self.map_path_to_action_list(knowledge.position, path[0])
@@ -66,16 +74,27 @@ class OurController(controller.Controller):
         except Exception as e:
             print(e)
         return action
-# naliczyć najkrótsze ścieżki po aktualizacji mapy oraz po dotarciu do punktu
-# dict z najkrótszymi ścieżkami.
+
+    # naliczyć najkrótsze ścieżki po aktualizacji mapy oraz po dotarciu do punktu
+    # dict z najkrótszymi ścieżkami.
+    def update_state(self, knowledge: characters.ChampionKnowledge):
+        self.current_position = knowledge.position
+        self.seen_tiles.update(dict((x[0], (x[1], self.epoch)) for x in knowledge.visible_tiles.items()))
+        self.epoch += 1
+        self.build_grid()
+        self.finder = AStarFinder()
+        self.look_up_for_menhir(knowledge.visible_tiles)
+        self.look_up_for_weapons(knowledge.visible_tiles)
 
     def map_path_to_action_list(self, current_position: Coords, path: List[GridNode]) -> List[characters.Action]:
         initial_facing = self.seen_tiles[current_position][0].character.facing
-        facings: List[characters.Facing] = list(map(lambda a: characters.Facing(Coords(a[1].x-a[0].x, a[1].y-a[0].y)), list(zip(path[:-1], path[1:]))))
+        facings: List[characters.Facing] = list(
+            map(lambda a: characters.Facing(Coords(a[1].x - a[0].x, a[1].y - a[0].y)), list(zip(path[:-1], path[1:]))))
         actions: List[characters.Action] = []
         for a, b in zip([initial_facing, *facings[:-1]], facings):
             actions.extend(self.map_facings_to_actions(a, b))
         return actions
+
     def map_facings_to_actions(self, f1: characters.Facing, f2: characters.Facing) -> List[characters.Action]:
         if f1 == f2:
             return [characters.Action.STEP_FORWARD]
@@ -90,7 +109,8 @@ class OurController(controller.Controller):
         pass
 
     def reset(self, arena_description: arenas.ArenaDescription) -> None:
-        print(arena_description) # dostajemy to także przed pierwszą turą - możemy zapamiętywać mapy między rozgrywkami po ich nazwie
+        print(
+            arena_description)  # dostajemy to także przed pierwszą turą - możemy zapamiętywać mapy między rozgrywkami po ich nazwie
         # reset roud unique variables
         self.epoch = 0
         self.actions = []
@@ -111,6 +131,47 @@ class OurController(controller.Controller):
             self.grid = Grid(MAX_SIZE, MAX_SIZE, walkable_tiles_matrix)
         except Exception as e:
             print(e)
+
+    def look_up_for_menhir(self, tiles: Dict[coordinates.Coords, tiles.TileDescription]):
+        if not self.menhir_chords:
+            for chords, tile in tiles.items():
+                if tile.type == 'menhir':
+                    self.menhir_chords = chords
+                    break
+
+    def look_up_for_weapons(self, tiles: Dict[coordinates.Coords, tiles.TileDescription]):
+        for chords, tile in tiles.items():
+            if self.wapons_chords[chords]:
+                self._update_waepon(chords, tile)
+            if tile.loot:
+                self._add_weapon(chords, tile)
+
+    def _add_weapon(self, chords, tile):
+        self.wapons_chords[chords] = SeenWeapon(tile.loot.name, self.epoch)
+
+    def _update_waepon(self, chords, tile):
+        if not tile.loot:
+            del self.wapons_chords[chords]
+
+    def find_mist(self, tiles: Dict[coordinates.Coords, tiles.TileDescription]) -> List[coordinates.Coords]:
+        mist_chords = []
+        for chords, tile in tiles.items():
+            for effect in tile.effects:
+                if effect.type == 'fog':
+                    mist_chords.append(chords)
+        return mist_chords
+
+    def find_nearest_mist_chords(self, mist_chords: List[coordinates.Coords]) -> Optional[coordinates.Coords]:
+        min_distance_squared = 2 * MAX_SIZE**2
+        nearest_mist_chords = None
+        for chords in mist_chords:
+            distance_squared = (chords.x - self.current_position.x) ** 2 + (chords.y - self.current_position.y) ** 2
+            if distance_squared < min_distance_squared:
+                min_distance_squared = distance_squared
+                nearest_mist_chords = chords
+        return min_distance_squared
+
+
 
 
     @property
