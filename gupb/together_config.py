35c95f6944bdb268f3139af8e87521ef1809ddc0
Paweł Hanusik <39835401+pawelhanusik@users.noreply.github.com>
2023-10-22 18:23:32 +0200

    Aragorn | Bot v1 (#1) (#234)
    * Bot v1 (#1)
    
    * Add bots from previos year
    
    * Add basic bot
    
    * Update name & avatar
    
    * Adapt together_config to new name
    
    * Add action description
    
    * Update memory each turn
    
    * Get info about facing
    
    * add action to fins nearest path to dest
    
    * Goto map center
    
    * info about tiles
    
    * add tabard for aragorn
    
    * Fix GotoAction
    
    * If cur pos = dst do other action
    
    * Add basic strategy
    
    * Delete old bots
    
    * Remove old bots from together config
    
    ---------
    
    Co-authored-by: Kamila <kamilasocko1@gmail.com>
    
    * Bot v1 (#2)
    
    * Collect potions
    
    ---------
    
    Co-authored-by: Kamila <kamilasocko1@gmail.com>
    
    ---------
    
    Co-authored-by: Kamila <kamilasocko1@gmail.com>
    Co-authored-by: Radosław Łazarz <radek.lazarz@gmail.com>

--- gupb/together_config.py
+++ gupb/together_config.py
@@ -2,12 +2,18 @@ from gupb.controller import random
 from gupb.scripts import arena_generator
 from gupb.controller import pat_i_kot
 
+from gupb.controller import aragorn
+
+
 CONFIGURATION = {
-    'arenas': arena_generator.generate_arenas(30),
+    'arenas': [
+        'ordinary_chaos',
+    ],
     'controllers': [
         random.RandomController("Alice"),
         random.RandomController("Bob"),
         random.RandomController("Cecilia"),
+        aragorn.AragornController("Aragorn"),
         pat_i_kot.PatIKotController("Kot i Pat")
     ],
     'start_balancing': False,
