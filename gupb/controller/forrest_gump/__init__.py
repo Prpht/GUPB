4468caf5848fb92faf50bd3d9a173f2cf1ec7662
Maksymilian Wojnar <38166640+m-wojnar@users.noreply.github.com>
2023-10-22 09:57:17 +0200

    Forrest Gump v1 (#226)

---
+++ gupb/controller/forrest_gump/__init__.py
@@ -0,0 +1,11 @@
+from .controller import ForrestGumpController
+
+
+__all__ = [
+    'ForrestGumpController',
+    'POTENTIAL_CONTROLLERS'
+]
+
+POTENTIAL_CONTROLLERS = [
+    ForrestGumpController('')
+]
