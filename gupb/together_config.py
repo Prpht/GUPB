9e6235ae4c48d88b7b375f422cbb7c6884a16c5f
SaraSwietek <115100071+SaraSwietek@users.noreply.github.com>
2023-10-22 17:21:07 +0200

    Pat i kot v1 (#227)

--- gupb/together_config.py
+++ gupb/together_config.py
@@ -1,5 +1,6 @@
 from gupb.controller import random
 from gupb.scripts import arena_generator
+from gupb.controller import pat_i_kot
 
 CONFIGURATION = {
     'arenas': arena_generator.generate_arenas(30),
@@ -7,6 +8,7 @@ CONFIGURATION = {
         random.RandomController("Alice"),
         random.RandomController("Bob"),
         random.RandomController("Cecilia"),
+        pat_i_kot.PatIKotController("Kot i Pat")
     ],
     'start_balancing': False,
     'visualise': False,
