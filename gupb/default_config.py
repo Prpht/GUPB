9c3a3bdb202ba0355b44c6c9183d779a2701f6ea
adratwa <agatadratwa.99@gmail.com>
2023-10-29 13:09:18 +0100

    Add a lof of new logic, describe it later

--- gupb/default_config.py
+++ gupb/default_config.py
@@ -1,22 +1,32 @@
 from gupb.controller import keyboard
 from gupb.controller import random
+from gupb.controller.cynamonka.cynamonka import CynamonkaController
+from gupb.model.arenas import ArenaDescription
 
-keyboard_controller = keyboard.KeyboardController()
+cynamonka_controller = CynamonkaController("CynamonkaController", arena_description=ArenaDescription('ordinary_chaos'))
 
 CONFIGURATION = {
     'arenas': [
         'ordinary_chaos',
     ],
     'controllers': [
-        keyboard_controller,
+        cynamonka_controller,
         random.RandomController("Alice"),
         random.RandomController("Bob"),
         random.RandomController("Cecilia"),
         random.RandomController("Darius"),
+        random.RandomController("Asd"),
+        random.RandomController("Bo"),
+        random.RandomController("Ce"),
+        random.RandomController("Dius"),
+        random.RandomController("Ace"),
+        random.RandomController("Bb"),
+        random.RandomController("C"),
+        random.RandomController("Das"),
     ],
     'start_balancing': False,
     'visualise': True,
-    'show_sight': keyboard_controller,
+    'show_sight': cynamonka_controller,
     'runs_no': 1,
     'profiling_metrics': [],
 }
