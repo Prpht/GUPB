a2828218189659f9416d8f0f75c5e9ac9da6eaf8
Wojciech Ciezobka <wciezobka@student.agh.edu.pl>
2023-11-04 19:57:04 +0100

    new directory structure for r2d2

---
+++ gupb/controller/r2d2/__init__.py
@@ -0,0 +1,10 @@
+from .reckless_roaming_dancing_druid import RecklessRoamingDancingDruid
+
+__all__ = [
+    'RecklessRoamingDancingDruid',
+    'POTENTIAL_CONTROLLERS'
+]
+
+POTENTIAL_CONTROLLERS = [
+    RecklessRoamingDancingDruid('R2D2')
+]
