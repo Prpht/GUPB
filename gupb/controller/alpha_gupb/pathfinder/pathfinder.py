79a4ddca82ae6a203c7bcc262bf9033db4bdde02
Wojciech Fałat <68254702+Waloott@users.noreply.github.com>
2023-11-05 13:27:38 +0100

    Alpha v2 (#253)
    * AlphaGUPB v0
    
    * alpha v2
    
    ---------
    
    Co-authored-by: MrSteradian360 <77154805+MrSteradian360@users.noreply.github.com>

---
+++ gupb/controller/alpha_gupb/pathfinder/pathfinder.py
@@ -0,0 +1,75 @@
+from queue import PriorityQueue
+
+class Node():
+    def __init__(self, parent=None, position=None):
+        self.parent = parent
+        self.position = position
+
+        self.g = 0
+        self.h = 0
+        self.f = 0
+
+    def __eq__(self, other):
+        return self.position == other.position
+
+def is_walkable(blocks, x, y):
+    for block in blocks:
+        if block['x'] == x and block['y'] == y:
+            return block['walkable']
+    return True  # Default to walkable if position not found in blocks
+
+def astar(blocks, start, end):
+    start_node = Node(None, start)
+    end_node = Node(None, end)
+
+    open_list = []
+    closed_list = []
+
+    open_list.append(start_node)
+    i = 0
+    while open_list and i<200:
+        i+=1
+        current_node = open_list[0]
+        current_index = 0
+        for index, item in enumerate(open_list):
+            if item.f < current_node.f:
+                current_node = item
+                current_index = index
+
+        open_list.pop(current_index)
+        closed_list.append(current_node)
+
+        if current_node == end_node:
+            path = []
+            current = current_node
+            while current is not None:
+                path.append(current.position)
+                current = current.parent
+            return path[::-1]
+
+        children = []
+        # Only move left, right, up, or down (no diagonals)
+        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0)]:
+            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+
+            # Check if it's walkable
+            if not is_walkable(blocks, node_position[0], node_position[1]):
+                continue
+
+            new_node = Node(current_node, node_position)
+            children.append(new_node)
+
+        for child in children:
+            if len([closed_child for closed_child in closed_list if closed_child == child]) > 0:
+                continue
+
+            child.g = current_node.g + 1
+            child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
+            child.f = child.g + child.h
+
+            if len([open_node for open_node in open_list if child == open_node and child.g > open_node.g]) > 0:
+                continue
+
+            open_list.append(child)
+
+    return None
