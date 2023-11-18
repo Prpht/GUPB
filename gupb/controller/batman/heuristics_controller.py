bfa2b335d51e3957fc8855a614b15a335926787d
jupiterb <piotr.bialy.407@gmail.com>
2023-11-18 14:58:10 +0100

    strategies factory

--- gupb/controller/batman/heuristics_controller.py
+++ gupb/controller/batman/heuristics_controller.py
@@ -6,14 +6,7 @@ from gupb.model import arenas
 from gupb.controller.batman.heuristic.events import EventDetector
 from gupb.controller.batman.heuristic.navigation import Navigation
 from gupb.controller.batman.heuristic.passthrough import Passthrough
-from gupb.controller.batman.heuristic.strategies import (
-    DefendingStrategy,
-    FightingStrategy,
-    HidingStrategy,
-    RotatingStrategy,
-    RunningAwayStrategy,
-    ScoutingStrategy,
-)
+from gupb.controller.batman.heuristic.strategies import StrategiesFactory
 
 from gupb.controller.batman.knowledge.knowledge import Knowledge
 
@@ -27,18 +20,8 @@ class BatmanHeuristicsController(controller.Controller):
         self._name = name
         self._episode = 0
         self._game = 0
-        self._knowledge: Optional[Knowledge] = None
-
-        # TODO replace with statemachine?
-        self._strategies = {
-            "scouting": ScoutingStrategy(),
-            "defending": DefendingStrategy(),
-        }
-        self._current_strategy = self._strategies["scouting"]
 
         self._event_detector = EventDetector()
-        self._navigation = None
-        self._passthrough = None
 
     def decide(self, knowledge: ChampionKnowledge) -> Action:
         assert (
@@ -56,7 +39,7 @@ class BatmanHeuristicsController(controller.Controller):
             action, strategy = self._current_strategy.decide(
                 self._knowledge, events, self._navigation
             )
-            self._current_strategy = self._strategies[strategy]
+            self._current_strategy = self._strategies.get(strategy)
             changed_strategies += 1
 
         # this should never happen (but it does happen xd)
@@ -75,15 +58,8 @@ class BatmanHeuristicsController(controller.Controller):
         self._knowledge = Knowledge(arena_description)
         self._navigation = Navigation(self._knowledge)
         self._passthrough = Passthrough(self._knowledge, self._navigation, samples=1000)
-        self._strategies = {
-            "defending": DefendingStrategy(),
-            "fighting": FightingStrategy(),
-            "hiding": HidingStrategy(self._passthrough),
-            "rotating": RotatingStrategy(),
-            "running_away": RunningAwayStrategy(),
-            "scouting": ScoutingStrategy(),
-        }
-        self._current_strategy = self._strategies["hiding"]
+        self._strategies = StrategiesFactory(self._passthrough)
+        self._current_strategy = self._strategies.get("hiding")
 
     @property
     def name(self) -> str:
