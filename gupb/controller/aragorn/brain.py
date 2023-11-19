ff0f9da08a8d5e41e57e0b7b5bd7f40f5cce4d23
korneliasusz <77573816+korneliasusz@users.noreply.github.com>
2023-11-19 11:52:49 +0100

    Aragorn v3 (#266)
    * Cleanup action code
    
    * For random map load everything as land
    
    * Fix map loading
    
    * Simplify pathfinding
    
    * Try to go to menhir
    
    * Fix going straight into mist
    
    * Add info when tiles were updated & improve going into menhir
    
    * Fix parsing descriptions
    
    * Fix potion distance calculating
    
    * Add new, simpler strategy
    
    * Cleanup brain
    
    * Checked order - is ok
    
    * Cleanup memory
    
    * Cleanup actions & add infitity to constants
    
    * Aragorn v2.0 (#3)
    
    * Cleanup action code
    
    * For random map load everything as land
    
    * Fix map loading
    
    * Simplify pathfinding
    
    * Try to go to menhir
    
    * Fix going straight into mist
    
    * Add info when tiles were updated & improve going into menhir
    
    * Fix parsing descriptions
    
    * Fix potion distance calculating
    
    * Add new, simpler strategy
    
    * Cleanup brain
    
    * Checked order - is ok
    
    * Cleanup memory
    
    * Cleanup actions & add infitity to constants
    
    * Finding nearest weapon
    
    * add spider skin
    
    * Add debugging to brain
    
    * Fix rotating while walking to nearest oponent
    
    * fix getting to closest weapon
    
    * take weapon when it's worth
    
    * set knife in the highest priority for now
    
    * Add cost to path for tiles with menhir & in range of enemies
    
    * Debug info disabled
    
    ---------
    
    Co-authored-by: Pawel Hanusik <39835401+pawelhanusik@users.noreply.github.com>
    Co-authored-by: Kamila <kamilasocko1@gmail.com>

--- gupb/controller/aragorn/brain.py
+++ gupb/controller/aragorn/brain.py
@@ -3,7 +3,7 @@ from gupb.model import arenas, characters, coordinates, weapons
 from gupb.controller.aragorn.memory import Memory
 from gupb.controller.aragorn.actions import *
 from gupb.controller.aragorn import utils
-from gupb.controller.aragorn.constants import DEBUG, INFINITY
+from gupb.controller.aragorn.constants import DEBUG, INFINITY, OUR_BOT_NAME
 
 
 
@@ -15,17 +15,24 @@ class Brain:
         self.memory.update(knowledge)
 
         actions = []
+        if DEBUG: dbg_ac_msgs = []
 
         if self.memory.willGetIdlePenalty():
+            # TODO: allow to decide action, afterwards, if pos will no change - force spin
+            if DEBUG: dbg_ac_msgs.append("Spinning to prevent idle penalty")
             spinAction = SpinAction()
             spinAction.setSpin(characters.Action.TURN_LEFT)
             actions.append(spinAction)
 
+        # ATTACKING
+
         if self.memory.hasOponentInFront():
+            if DEBUG: dbg_ac_msgs.append("Attacking, since got oponent in front")
             attackAction = AttackAction()
             actions.append(attackAction)
 
         if self.memory.hasOponentOnRight():
+            if DEBUG: dbg_ac_msgs.append("Attacking, since got oponent on right")
             spinAction = SpinAction()
             spinAction.setSpin(characters.Action.TURN_RIGHT)
             actions.append(spinAction)
@@ -34,6 +41,7 @@ class Brain:
             actions.append(attackAction)
 
         if self.memory.hasOponentOnLeft():
+            if DEBUG: dbg_ac_msgs.append("Attacking, since got oponent on left")
             spinAction = SpinAction()
             spinAction.setSpin(characters.Action.TURN_LEFT)
             actions.append(spinAction)
@@ -41,32 +49,51 @@ class Brain:
             attackAction = AttackAction()
             actions.append(attackAction)
 
+        # ------------------------------------------
+
+        # PICKING STUFF
+        
+        # potion
         [closestPotionDistance, closestPotionCoords] = self.memory.getDistanceToClosestPotion()
 
         if closestPotionDistance is not None and closestPotionDistance < 8:
+            if DEBUG: dbg_ac_msgs.append("Picking nearby potion")
             goToPotionAction = GoToAction()
             goToPotionAction.setDestination(closestPotionCoords)
             actions.append(goToPotionAction)
 
         [closestWeaponDistance, closestWeaponCoords] = self.memory.getDistanceToClosestWeapon()
-
+        
+        # weapon
         if closestWeaponDistance is not None and closestWeaponDistance < 8:
+            if DEBUG: dbg_ac_msgs.append("Picking nearby weapon")
             goToWeaponAction = GoToAction()
             goToWeaponAction.setDestination(closestWeaponCoords)
             actions.append(goToWeaponAction)
-                
+        
+        # ------------------------------------------
+
+        # MIST FORCED MOVEMENT
+
         [menhirPos, prob] = self.memory.map.menhirCalculator.approximateMenhirPos(self.memory.tick)
 
         if menhirPos is not None and utils.coordinatesDistance(self.memory.position, menhirPos) > self.memory.map.mist_radius / 2:
+            if DEBUG: dbg_ac_msgs.append("Going closer to menhir")
             goToAroundAction = GoToAroundAction()
             goToAroundAction.setDestination(menhirPos)
             actions.append(goToAroundAction)
         
+        # ------------------------------------------
+
         closestEnemy = None
         closestEnemyDistance = INFINITY
 
         for coords in self.memory.map.terrain:
-            if self.memory.map.terrain[coords].character is not None and self.memory.position != coords:
+            if (
+                self.memory.map.terrain[coords].character is not None
+                and self.memory.map.terrain[coords].character.controller_name != OUR_BOT_NAME
+                and self.memory.position != coords
+            ):
                 distance = utils.coordinatesDistance(self.memory.position, coords)
                 
                 if distance < closestEnemyDistance:
@@ -74,21 +101,30 @@ class Brain:
                     closestEnemyDistance = distance
         
         if closestEnemy is not None:
+            if DEBUG: dbg_ac_msgs.append("Going closer to enemy")
             goToAttackAction = GoToAroundAction()
             goToAttackAction.setDestination(closestEnemy)
             actions.append(goToAttackAction)
 
-        
+        if DEBUG: dbg_ac_msgs.append("No action found, spinning")
         spinAction = SpinAction()
         actions.append(spinAction)
+        
+        # ------------------------------------------
+        
+        actionIndexPerformed = 0
         
         for action in actions:
             ret = action.perform(self.memory)
             
             if ret is not None and ret is not characters.Action.DO_NOTHING:
+                if DEBUG: print("[ARAGORN|BRAIN]", action.__class__.__name__, dbg_ac_msgs[actionIndexPerformed])
                 self.onDecisionReturning(ret)
                 return ret
+            
+            actionIndexPerformed += 1
         
+        if DEBUG: print("[ARAGORN|BRAIN] None of actions returned anything, spinning")
         self.onDecisionReturning(characters.Action.TURN_RIGHT)
         return characters.Action.TURN_RIGHT
     
