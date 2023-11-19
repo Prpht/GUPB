f3919f8c77f86783f6568ec05c018129c8c0deb1
MrSteradian360 <77154805+MrSteradian360@users.noreply.github.com>
2023-11-19 11:55:43 +0100

    AlphaGUPB v3 (#271)
    * predator bot v1
    
    * predator v1.1
    
    * AlphaGUPB v0
    
    * alpha v2
    
    * alpha v3
    
    * Update AlphaGUPB.py
    
    prints commented
    
    * Delete gupb/controller/predator directory
    
    * Update AlphaGUPB.py
    
    * Update characters.py
    
    * Update render.py
    
    * Update default_config.py
    
    ---------
    
    Co-authored-by: Waloott <68254702+Waloott@users.noreply.github.com>

--- gupb/default_config.py
+++ gupb/default_config.py
@@ -1,6 +1,7 @@
 from gupb.controller import keyboard
 from gupb.controller import random
 
+
 keyboard_controller = keyboard.KeyboardController()
 
 CONFIGURATION = {
@@ -8,17 +9,18 @@ CONFIGURATION = {
         'ordinary_chaos'
     ],
     'controllers': [
-        keyboard_controller,
+        # keyboard_controller,
         random.RandomController("Alice"),
         random.RandomController("Bob"),
         random.RandomController("Cecilia"),
         random.RandomController("Darius"),
         random.RandomController("Asd"),
         random.RandomController("Bo"),
-        random.RandomController("Ce"),
+        random.RandomController("Ce")
+
     ],
     'start_balancing': False,
-    'visualise': True,
+    'visualise': False,
     'show_sight': keyboard_controller,
     'runs_no': 1,
     'profiling_metrics': [],
