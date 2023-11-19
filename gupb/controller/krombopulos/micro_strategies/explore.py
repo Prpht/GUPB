3bf79774fe4d9337e60eeed48156cfdc837b8e02
Piotr Zawiślan <59446552+Byczong@users.noreply.github.com>
2023-11-19 16:46:09 +0100

    Krombopulos michael v3 (#274)
    * krombopulos michael v1
    
    * fix krombopulos skin
    
    * clean up errors and constant map
    
    * add trainer and environment
    
    * make maaaany changes
    
    * debug
    
    * start strategy praise
    
    * ja juz nawet nie wiem
    
    * kmv3
    
    ---------
    
    Co-authored-by: wsrtka <witold.serwatka@pm.me>
    Co-authored-by: Radosław Łazarz <radek.lazarz@gmail.com>

--- gupb/controller/krombopulos/micro_strategies/explore.py
+++ gupb/controller/krombopulos/micro_strategies/explore.py
@@ -16,21 +16,21 @@ class ExploreMicroStrat(MicroStrategy):
         # move if champion has not moved in 5 epochs
         if action := self.avoid_afk():
             return action, False
+
+        # decision overrides in the base class
+        if action := self.decide_override():
+            return action, True
+
         front_tile = self.knowledge_sources.get_tile_info_in_front_of()
-        actions_to_choose_from = []
-        probs = []
-        if front_tile.consumable:
-            return characters.Action.STEP_FORWARD, True
-        elif front_tile.character:
-            return characters.Action.ATTACK, True
-        elif front_tile.loot:
-            return characters.Action.STEP_FORWARD, True
 
         left_tile = self.knowledge_sources.get_tile_in_direction(
             self.knowledge_sources.players.own_player_facing.turn_left())
 
         right_tile = self.knowledge_sources.get_tile_in_direction(
             self.knowledge_sources.players.own_player_facing.turn_right())
+
+        actions_to_choose_from = []
+        probs = []
 
         # queued action
         if self.queued_action:
@@ -41,22 +41,30 @@ class ExploreMicroStrat(MicroStrategy):
         # stuck in a hole
         if left_tile and left_tile.type in ('sea', 'wall') and front_tile.type in ('sea', 'wall'):
             self.queued_action = characters.Action.TURN_RIGHT
-            return characters.Action.TURN_RIGHT, True
+            return characters.Action.STEP_RIGHT, True
         elif right_tile and right_tile.type in ('sea', 'wall') and front_tile.type in ('sea', 'wall'):
             self.queued_action = characters.Action.TURN_LEFT
-            return characters.Action.TURN_LEFT, True
+            return characters.Action.STEP_LEFT, True
+        elif (left_tile and left_tile.type in ('sea', 'wall') and front_tile.type in ('sea', 'wall') and
+              right_tile and right_tile.type in ('sea', 'wall')):
+            self.queued_action = characters.Action.TURN_RIGHT
+            return characters.Action.STEP_BACKWARD, True
 
         if front_tile.type in ('land', 'menhir'):
             actions_to_choose_from.append(characters.Action.STEP_FORWARD)
-            probs.append(0.3)
+            probs.append(0.2)
 
         if left_tile and left_tile.type in ('land', 'menhir') or not left_tile:
-            actions_to_choose_from.append(characters.Action.TURN_LEFT)
+            actions_to_choose_from.append(characters.Action.STEP_LEFT)
             probs.append(0.1)
+            actions_to_choose_from.append(characters.Action.TURN_LEFT)
+            probs.append(0.05)
 
         if right_tile and right_tile.type in ('land', 'menhir') or not right_tile:
-            actions_to_choose_from.append(characters.Action.TURN_RIGHT)
+            actions_to_choose_from.append(characters.Action.STEP_RIGHT)
             probs.append(0.1)
+            actions_to_choose_from.append(characters.Action.TURN_RIGHT)
+            probs.append(0.05)
 
         actions_to_choose_from.append(characters.Action.ATTACK)
         probs.append(0.05)
