c58798add3d10af0beb1ee99d693d4148efb0b74
ewier <e.wierciak@gmail.com>
2023-11-26 15:23:39 +0100

    Ares (#285)
    * update from dev
    
    * cleaning
    
    * ROUND3: last minute update
    
    * fixing update function
    
    * ROUND2: improvements
    
    * ROUND3: pathfinding works
    
    * attacks by weapons and more movements
    
    * update from dev
    
    * DEV_ROUND4: rudimentary mist avoidance, weapons and other changes
    
    ---------
    
    Co-authored-by: Justyna <justyna12aneta@gmail.com>
    Co-authored-by: Justyna <75282333+Justyna-M@users.noreply.github.com>

--- gupb/custom_together_config.py
+++ gupb/custom_together_config.py
@@ -6,40 +6,36 @@ from gupb.controller import ares
 from gupb.controller import bob
 from gupb.controller import cynamonka
 from gupb.controller import forrest_gump
-from gupb.controller import frog
 from gupb.controller import krombopulos
 from gupb.controller import maly_konik
 from gupb.controller import mongolek
 from gupb.controller import pat_i_kot
 from gupb.controller import random
 from gupb.controller import roger
-from gupb.controller import r2d2
+# from gupb.controller import reckless_roaming_dancing_druid
 from gupb.scripts import arena_generator
 
 CONFIGURATION = {
-    'arenas': arena_generator.generate_arenas(50, arena_generator.random_size_generator()),
+    'arenas': arena_generator.generate_arenas(1),
     'controllers': [
-        # alpha_gupb.AlphaGUPB("AlphaGUPB"),
+        alpha_gupb.AlphaGUPB("AlphaGUPB"),
         ancymon.AncymonController("Ancymon"),
         aragorn.AragornController("AragornController"),
         ares.AresController("Nike"),
         bob.FSMBot(),
         # batman.BatmanHeuristicsController('Batman'),
         cynamonka.CynamonkaController("Cynamonka"),
-        # forrest_gump.ForrestGumpController("Forrest Gump"),
-        # frog.FrogController('Frog'),
+        forrest_gump.ForrestGumpController("Forrest Gump"),
         krombopulos.KrombopulosMichaelController(),
         maly_konik.MalyKonik("LittlePonny"),
-        # mongolek.Mongolek('Mongolek'),
+        mongolek.Mongolek('Mongolek'),
         pat_i_kot.PatIKotController("Kot i Pat"),
         random.RandomController("Alice"),
-        random.RandomController("Bob"),
-        random.RandomController("Celia"),
-        # r2d2.RecklessRoamingDancingDruid("R2D2"),
-        # roger.Roger('1'),
+        # reckless_roaming_dancing_druid.RecklessRoamingDancingDruid("R2D2"),
+        roger.Roger('1'),
     ],
     'start_balancing': False,
     'visualise': True,
-    'show_sight': False,
+    'show_sight': ares.AresController("Nike"),
     'runs_no': 1,
 }
