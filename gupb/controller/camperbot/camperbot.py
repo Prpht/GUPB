7ac6d372ae9f2cc1ef725a3fcff6bcc849928478
wozniakos10 <wozniakos10@gmail.com>
2025-04-12 15:19:59 +0200

    Improve tactic

--- gupb/controller/camperbot/camperbot.py
+++ gupb/controller/camperbot/camperbot.py
@@ -1,6 +1,6 @@
 from gupb import controller
 from gupb.model import arenas
-from gupb.model import characters
+from gupb.model import characters, tiles
 import random
 import networkx as nx
 from gupb.model.coordinates import Coords
@@ -31,6 +31,27 @@ class CamperBotController(controller.Controller):
         self.arena = None
         self.arena_graph = None
         self.path_to_menhir = None
+        self.forrest_tiles_list = []
+        self.path_to_forrest = None
+        self.go_to_forrest = True
+        self.go_to_menhir = False
+
+    def is_mist_arround(self, knowledge: characters.ChampionKnowledge, mist_arround_trehshold: int = 2) -> bool:
+        for coords, visible_tile in knowledge.visible_tiles.items():
+            effects = visible_tile.effects
+            for elem in effects:
+                if elem.type == "mist":
+                    current_position = knowledge.position
+                    distance_from_mnist = self.manhattan_distance(current_position, Coords(coords[0], coords[1]))
+                    if distance_from_mnist < mist_arround_trehshold:
+                        return True
+        return False
+
+    def manhattan_distance(self, pos1: Coords, pos2: Coords) -> int:
+        # print(f"calcualting manhattan distance between {pos1}, {pos2}")
+        dist = abs(pos1.x - pos2.x) + abs(pos1.y - pos2.y)
+        # print(f"dist: {dist}")
+        return dist
 
     def load_arena_to_graph(self):
         self.arena = arenas.Arena.load(self.arena_description.name)
@@ -43,6 +64,9 @@ class CamperBotController(controller.Controller):
             # Sprawdzamy tylko sąsiadów jeśli pole jest przechodnie
             if not tile.terrain_passable():
                 continue
+
+            if tile.description().type == "forest":
+                self.forrest_tiles_list.append(coord)
 
             neighbors = [
                 Coords(coord.x + 1, coord.y),
@@ -72,20 +96,32 @@ class CamperBotController(controller.Controller):
                 self.is_menhir_found = True
                 self.menhir_cords = coords
 
+
+    def find_path(self, source: Coords, target: Coords):
+        try:
+            path = nx.shortest_path(self.arena_graph, source=source, target=target)
+            return path[1:]
+        except nx.NetworkXNoPath:
+            return None
+        except nx.NodeNotFound as e:
+            return None
+
     def find_path_to_menhir(self, knowledge: characters.ChampionKnowledge):
         current_position = knowledge.position
         menhir_position = self.menhir_cords
+        self.path_to_menhir =  self.find_path(current_position, menhir_position)
 
-        try:
-            path = nx.shortest_path(self.arena_graph, source=knowledge.position, target=self.menhir_cords)
-            # print(f"Path to menhir found from current position {knowledge.position}: {path}")
-            self.path_to_menhir = path[1:]
-        except nx.NetworkXNoPath:
-            # print(f"Brak ścieżki z {current_position} do {menhir_position}")
-            return None
-        except nx.NodeNotFound as e:
-            # print(f"Nie znaleziono węzła: {e}")
-            return None
+    def find_path_to_forrest(self, knowledge: characters.ChampionKnowledge):
+        current_position = knowledge.position
+        closest_forrest  = sorted(self.forrest_tiles_list, key=lambda x: self.manhattan_distance(current_position, x))[0]
+        self.path_to_forrest = self.find_path(current_position, closest_forrest)
+
+
+    def is_tile_mist(self, tile: tiles.TileDescription) -> bool:
+        for elem in tile.effects:
+            if elem.type == "mist":
+                return True
+        return False
 
     def decide(self, knowledge: characters.ChampionKnowledge) -> characters.Action:
         current_position = knowledge.position
@@ -99,6 +135,9 @@ class CamperBotController(controller.Controller):
         possible_step_forward = current_position + self.step_forward(facing)
         possible_step_forward = Coords(possible_step_forward[0], possible_step_forward[1])
 
+        possible_step_backward = current_position + self.step_backward(facing)
+        possible_step_backward = Coords(possible_step_backward[0], possible_step_backward[1])
+
         possible_step_right = current_position + self.step_right(facing)
         possible_step_right = Coords(possible_step_right[0], possible_step_right[1])
 
@@ -109,10 +148,25 @@ class CamperBotController(controller.Controller):
         secondary_actions = TURNING_ACTIONS.copy()
 
         if not self.is_menhir_found:
-
             tile_forward = knowledge.visible_tiles.get(possible_step_forward, None)
             tile_right = knowledge.visible_tiles.get(possible_step_right, None)
             tile_left = knowledge.visible_tiles.get(possible_step_left, None)
+            tile_backward = knowledge.visible_tiles.get(possible_step_backward, None)
+
+            if self.is_mist_arround(knowledge):
+                if tile_forward and not self.is_tile_mist(tile_forward):
+                    return characters.Action.STEP_FORWARD
+
+                if tile_backward and not self.is_tile_mist(tile_backward):
+                    return characters.Action.STEP_BACKWARD
+
+                if tile_right and not self.is_tile_mist(tile_right):
+                    return characters.Action.STEP_RIGHT
+
+                if tile_left and not self.is_tile_mist(tile_left):
+                    return characters.Action.STEP_LEFT
+                #TODO: find a better way to escape form mnist
+
 
             if tile_forward and tile_forward.character:
                 return characters.Action.ATTACK
@@ -147,20 +201,47 @@ class CamperBotController(controller.Controller):
                 return characters.Action.STEP_BACKWARD
 
         else:
-            if self.path_to_menhir is None:
-                self.find_path_to_menhir(knowledge)
 
-            # condition to stop going towards menhir as we are close
-            if len(self.path_to_menhir) > 1:
-                next_tile = self.path_to_menhir.pop(0)
-                next_step_action = self.step_to_menhir(knowledge, next_tile)
-                return next_step_action
-            # spinning around when we are close to menhir
-            else:
-                return random.choice(TURNING_ACTIONS)
+            # Going to forrest
+            if self.go_to_forrest:
+                #If we are in forrest na mnist arround - go to menhir
+                if self.is_mist_arround(knowledge):
+                    self.find_path_to_menhir(knowledge)
+                    next_tile = self.path_to_menhir.pop(0)
+                    next_step_action = self.step_to_target(knowledge, next_tile)
+                    self.go_to_forrest = False
+                    self.go_to_menhir = True
+                    return next_step_action
 
-    def step_to_menhir(self, knowledge: characters.ChampionKnowledge, next_position: Coords):
-        "Chosing action to step toward menhir"
+                if self.path_to_forrest is None:
+                    self.find_path_to_forrest(knowledge)
+                # condition to stop going towards menhir as we are close
+                if len(self.path_to_forrest) > 0:
+                    next_tile = self.path_to_forrest.pop(0)
+                    next_step_action = self.step_to_target(knowledge, next_tile)
+                    return next_step_action
+                # spinning around when we are close to menhir
+                else:
+                    return characters.Action.TURN_RIGHT
+
+            # Going to menhir
+            if self.go_to_menhir:
+                if self.path_to_menhir is None:
+                    self.find_path_to_menhir(knowledge)
+
+                #Slow up going to menhir, until mist is close enough
+                if self.is_mist_arround(knowledge):
+                    # condition to stop going towards menhir as we are close
+                    if len(self.path_to_menhir) > 1:
+                        next_tile = self.path_to_menhir.pop(0)
+                        next_step_action = self.step_to_target(knowledge, next_tile)
+                        return next_step_action
+                    # spinning around when we are close to menhir
+                else:
+                    return random.choice(TURNING_ACTIONS)
+
+    def step_to_target(self, knowledge: characters.ChampionKnowledge, next_position: Coords):
+        "Chosing action to step toward target"
         current_position = knowledge.position
         character = knowledge.visible_tiles[knowledge.position].character
         facing = character.facing
@@ -280,6 +361,10 @@ class CamperBotController(controller.Controller):
         self.arena = None
         self.path_to_menhir = None
         self.arena_graph = None
+        self.path_to_forrest = None
+        self.forrest_tiles_list = []
+        self.go_to_menhir = False
+        self.go_to_forrest = True
         self.load_arena_to_graph()
 
 
