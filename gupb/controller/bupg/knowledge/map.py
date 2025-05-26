f9a174c75daa8fc5034a1ecc1c07bfc166a4c244
Patryk Kożuch <patryk.kozuch.1@gmail.com>
2025-05-26 23:31:22 +0200

    Finish v2

--- gupb/controller/bupg/knowledge/map.py
+++ gupb/controller/bupg/knowledge/map.py
@@ -3,11 +3,12 @@ from typing import Optional
 import numpy as np
 import scipy.ndimage
 
+from gupb.controller.bupg.utils import weapon_class
 from gupb.model import characters
 from gupb.model.arenas import Terrain, terrain_size
+from gupb.model.characters import ChampionDescription
 from gupb.model.coordinates import Coords
-from gupb.model.effects import EffectDescription
-from gupb.model.games import MIST_TTH_PER_CHAMPION
+from gupb.model.effects import EffectDescription, Fire, FIRE_DAMAGE
 
 
 class MapKnowledge:
@@ -15,13 +16,14 @@ class MapKnowledge:
         self.terrain = terrain
         self.menhir_location: Optional[Coords] = None
         self.mnist_moved = False
-        self.estimated_menhir_location: np.ndarray | None = None
         self._total_weight: float = 0
         self._mist_radius: int | None = None
         self._episode: int = 0
         self.looked_at = None
-        self.opponents: dict[Coords, int] = {}
+        self.opponents: dict[Coords, tuple[ChampionDescription, int]] = {}
         self.timestamp = 0
+        W, H = terrain_size(terrain)
+        self.danger_map = np.zeros(shape=(H, W), dtype=np.int8)
 
         # All these attributes MIGHT BE OUTDATED
         self.weapons = {
@@ -31,7 +33,7 @@ class MapKnowledge:
         }
 
         self.consumables = {
-            coords: tile.consumable.description()
+           coords: tile.consumable.description()
             for coords, tile in self.terrain.items()
             if tile.consumable
         }
@@ -39,6 +41,12 @@ class MapKnowledge:
         self.effects: dict[Coords, EffectDescription] = {}
         self.mist: set[Coords] = set()
         self.fires: set[Coords] = set()
+
+        self.trees = {
+            coords: tile.description()
+            for coords, tile in self.terrain.items()
+            if tile.description().type == 'forest'
+        }
 
     def remove_unreachable_weapons(self):
         weapons_to_remove = []
@@ -58,7 +66,10 @@ class MapKnowledge:
 
     def update_terrain(self, knowledge: characters.ChampionKnowledge):
         self.looked_at[knowledge.position[1], knowledge.position[0]] = 0
+        self.danger_map = np.zeros(shape=self.danger_map.shape, dtype=np.int8)
+
         for coords, tile in knowledge.visible_tiles.items():
+            coords = Coords(*coords)
             self.looked_at[coords[1], coords[0]] = 0
 
             # Update weapons
@@ -86,62 +97,44 @@ class MapKnowledge:
             if tile.type == "menhir":
                 self.menhir_location = coords
 
-            if tile.character:
-                self.opponents[coords] = self.timestamp
+            if tile.character and tile.character.controller_name != "BUPG BUPG":
+                for c, (o, t) in self.opponents.items():
+                    if o.controller_name == tile.character.controller_name:
+                        del self.opponents[c]
+                        break
+                self.opponents[coords] = (tile.character, self.timestamp)
+
+        opponents_to_remove = []
+        for coords, (opponent, t) in self.opponents.items():
+            if self.timestamp - t > 3:
+                opponents_to_remove.append(coords)
+
+        for coords in opponents_to_remove:
+            del self.opponents[coords]
+
+        for c, (o, t) in self.opponents.items():
+            wpn = weapon_class(o.weapon.name)
+            danger_tiles = set(knowledge.visible_tiles) & set(
+                wpn.cut_positions(self.terrain, Coords(*c), o.facing)
+                ) & set(self.terrain)
+
+            for x, y in danger_tiles:
+                eff = wpn.cut_effect()
+                self.danger_map[y, x] = eff.damage if not isinstance(eff, Fire) else FIRE_DAMAGE
 
         self.timestamp += 1
 
+        for coords in self.mist:
+            self.looked_at[coords.y, coords.x] = 0
+            self.danger_map[coords.y, coords.x] += 1
 
-    def episode_tick(self):
-        if self._mist_radius is None:
-            size = terrain_size(self.terrain)
-            self._mist_radius = int(size[0] * 2 ** 0.5) + 1
-
-        self._episode += 1
-        self.mnist_moved = self._episode % MIST_TTH_PER_CHAMPION == 0
-        if self.mnist_moved:
-            self._mist_radius -= 1
-
-    def update_menhir_location(self, new_estimate: np.ndarray, weight: float = 1.0) -> None:
-        if self.estimated_menhir_location is None:
-            self.estimated_menhir_location = np.zeros((2,))
-        self._total_weight += weight
-        self.estimated_menhir_location += weight * (new_estimate - self.estimated_menhir_location) / self._total_weight
+        for coords in self.fires:
+            self.danger_map[coords.y, coords.x] += FIRE_DAMAGE
 
     def get_most_unknown_point(self):
         distance_map = scipy.ndimage.distance_transform_edt(self.looked_at)
         arg = np.unravel_index(np.argmax(distance_map), shape=self.looked_at.shape)
         return arg[1], arg[0]
-
-    def find_closest_weapon(self, position: Coords, weapon_type: str):
-        x, y = position
-        min = float("inf")
-        coords = None
-        for (w_x, w_y), desc in self.weapons.items():
-            if desc.name != weapon_type:
-                continue
-
-            dist = abs(x - w_x) + abs(y - w_y)
-            if dist < min:
-                min = dist
-                coords = Coords(w_x, w_y)
-
-        return coords
-
-    def find_closest_tree(self, menhir_position: Coords):
-        x, y = menhir_position
-        min = float("inf")
-        coords = None
-        for (w_x, w_y), tile in self.terrain.items():
-            if tile.description().type != 'forest':
-                continue
-
-            dist = abs(x - w_x) + abs(y - w_y)
-            if dist < min:
-                min = dist
-                coords = Coords(w_x, w_y)
-
-        return coords
 
     def distance_to_mist(self, position: Coords):
         x, y = position
@@ -152,15 +145,3 @@ class MapKnowledge:
                 min = dist
 
         return min
-
-    def distance_to_potion(self, position: Coords):
-        x, y = position
-        min = float("inf")
-        coords = None
-        for w_x, w_y in self.consumables:
-            dist = abs(x - w_x) + abs(y - w_y)
-            if dist < min:
-                min = dist
-                coords = Coords(w_x, w_y)
-
-        return min, coords
