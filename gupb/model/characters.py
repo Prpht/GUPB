cf919c83c47bddd527aced85414a0a5160882a54
Tomasz Smyda <tomasz.smyda@gmail.com>
2026-03-20 21:44:37 +0100

    Add TheTrooper Sprite

--- gupb/model/characters.py
+++ gupb/model/characters.py
@@ -36,6 +36,7 @@ class ChampionDescription(NamedTuple):
 class Tabard(Enum):
     BLUE = 'Blue'
     BENJAMIN_NETANYAHU = 'Benjamin Netanyahu'
+    THE_TROOPER = 'The Trooper'
     BROWN = 'Brown'
     GREY = 'Grey'
     GREEN = 'Green'
