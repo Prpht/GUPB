5220e60b64353a1277a4e8e73669b1ab8f0b7d17
Jakub Grześ <117384817+jgrzes@users.noreply.github.com>
2026-03-24 22:02:51 +0100

    Add BladeRunner controller (#364)

---
+++ gupb/controller/blade_runner/blade_runner.py
@@ -0,0 +1,32 @@
+from gupb import controller
+from gupb.model import arenas
+from gupb.model import characters
+
+class BladeRunner(controller.Controller):
+    def __init__(self, first_name: str):
+        self.first_name: str = first_name
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, BladeRunner):
+            return self.first_name == other.first_name
+        return False
+
+    def __hash__(self) -> int:
+        return hash(self.first_name)
+
+    def decide(self, knowledge: characters.ChampionKnowledge) -> characters.Action:
+        return characters.Action.TURN_LEFT
+
+    def praise(self, score: int) -> None:
+        pass
+
+    def reset(self, game_no: int, arena_description: arenas.ArenaDescription) -> None:
+        pass
+
+    @property
+    def name(self) -> str:
+        return self.first_name
+
+    @property
+    def preferred_tabard(self) -> characters.Tabard:
+        return characters.Tabard.RED
