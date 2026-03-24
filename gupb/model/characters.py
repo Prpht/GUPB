9a2d1289365bf4fb0f518df0d4c72952ca2f92dc
Grzegorz <106807538+Grzmro@users.noreply.github.com>
2026-03-24 22:22:34 +0100

    [Pudzian] - register (#372)
    Co-authored-by: Radosław Łazarz <radek.lazarz@gmail.com>

--- gupb/model/characters.py
+++ gupb/model/characters.py
@@ -36,6 +36,7 @@ class ChampionDescription(NamedTuple):
 class Tabard(Enum):
     BLUE = 'Blue'
     BENJAMIN_NETANYAHU = 'Benjamin Netanyahu'
+    PUDZIAN = 'Pudzian'
     THE_TROOPER = 'The Trooper'
     BOB = 'Bob'
     BROWN = 'Brown'
