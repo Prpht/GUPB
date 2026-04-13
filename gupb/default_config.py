d536958f7c7c36d3237f315a424803ea55079e84
Tomasz Smyda <tomasz.smyda@gmail.com>
2026-04-14 01:54:13 +0200

    Register TheTrooper in default_config.py

--- gupb/default_config.py
+++ gupb/default_config.py
@@ -1,6 +1,7 @@
 from gupb.controller import keyboard
 from gupb.controller import random
 from gupb.controller import bigbot
+from gupb.controller.the_trooper import TheTrooper
 
 keyboard_controller = keyboard.KeyboardController()
 
@@ -15,6 +16,7 @@ CONFIGURATION = {
         random.RandomController("Darius"),
         keyboard_controller,
         bigbot.BIGbot("BIGbot"),
+        TheTrooper("The Trooper"),
     ],
     'start_balancing': False,
     'visualise': True,
