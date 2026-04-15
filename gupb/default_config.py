8a958abfb66cd768bbbb3ced60c7daa16c14808b
Krzysztof Kapera <63041828+krzkapera@users.noreply.github.com>
2026-04-15 13:35:31 +0200

    Round 1 - Syntax Terror (#378)
    * round1
    
    * round1 v2
    
    * round1 v3
    
    * restore name
    
    ---------
    
    Co-authored-by: Radosław Łazarz <radek.lazarz@gmail.com>

--- gupb/default_config.py
+++ gupb/default_config.py
@@ -1,5 +1,6 @@
 from gupb.controller import keyboard
 from gupb.controller import random
+from gupb.controller import syntax_terror
 from gupb.controller import bigbot
 from gupb.controller.the_trooper import TheTrooper
 from gupb.controller.benjamin_netanyahu import BenjaminNetanyahu
@@ -16,6 +17,8 @@ CONFIGURATION = {
         random.RandomController("Bob"),
         random.RandomController("Cecilia"),
         random.RandomController("Darius"),
+        keyboard_controller,
+        syntax_terror.SyntaxTerror("Syntax Terror"),
         # keyboard_controller,
         bigbot.BIGbot("BIGbot"),
         TheTrooper("The Trooper"),
