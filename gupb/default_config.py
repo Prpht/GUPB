01d87c4ae8a9f19f2a996dabc9015e657d94c13e
Szymon Paja <69346711+pajka1sz@users.noreply.github.com>
2026-04-15 13:54:13 +0200

    Bob ready for first round (#381)
    * Added Bob bot
    
    * Bot appearance updated
    
    * Bot appearance updated #2
    
    * Bob ready for first round
    
    ---------
    
    Co-authored-by: Radosław Łazarz <radek.lazarz@gmail.com>

--- gupb/default_config.py
+++ gupb/default_config.py
@@ -2,6 +2,7 @@ from gupb.controller import keyboard
 from gupb.controller import random
 from gupb.controller import syntax_terror
 from gupb.controller import bigbot
+from gupb.controller import bob
 from gupb.controller.jeffrey_e.jeffrey_e_controller import JeffreyEController
 from gupb.controller.the_trooper import TheTrooper
 from gupb.controller.benjamin_netanyahu import BenjaminNetanyahu
@@ -22,6 +23,7 @@ CONFIGURATION = {
         syntax_terror.SyntaxTerror("Syntax Terror"),
         # keyboard_controller,
         bigbot.BIGbot("BIGbot"),
+        bob.Bob("BobMinion"),
         JeffreyEController("JeffreyE")
         TheTrooper("The Trooper"),
     ],
